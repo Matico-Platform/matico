1dfd8899a8c7fee7c164b38ada7b3f6cff58eb5f
Stuart Lynn <stuart.lynn@gmail.com>
2021-01-20 12:44:09 -0500

    Using progress bar and uploader in UploadForm component

--- simple-map-viewer/src/components/UploadForm/UploadForm.tsx
+++ simple-map-viewer/src/components/UploadForm/UploadForm.tsx
@@ -1,17 +1,67 @@
-import React, {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import { Uploader } from '../Uploader/Uploader';
+import { Card } from '../Card/Card';
+import { uploadFile } from '../../api';
+import { Tabs, Tab } from '../Tabs/Tabs';
 
+type Props = {};
 
-type Props ={
+export const UploadForm: React.FC<Props> = ({}: Props) => {
+    const [progress, setProgress] = useState(0);
+    const [files, setFiles] = useState<FileList | null>(null);
+    const [startUpload, setStartUpload] = useState<boolean>(false);
 
-}
+    const selectFiles = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.files);
+        setFiles(e.target.files);
+    };
 
-export const UploadForm : React.FC<Props> = ({}:Props)=>{
-    return(
-        <div className='upload-form'>
-            <form target="http://localhost:8080/upload" method="post" encType="multipart/form-data">
-            <input type="file" multiple name="file" />
-            <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
-}
+    const upload = () => {
+        setStartUpload(true);
+    };
+
+    return (
+        <Card>
+            <Tabs>
+                <Tab name="Upload Files">
+                    {files && (
+                        <ul>
+                            {Array.from(files).map((f) => (
+                                <li>
+                                    {startUpload ? (
+                                        <Uploader
+                                            key={f.name}
+                                            url="/upload"
+                                            file={f}
+                                        />
+                                    ) : (
+                                        <>
+                                            {f.name} :
+                                            {(
+                                                f.size * 1e-6
+                                            ).toPrecision(2)}
+                                            Mb
+                                        </>
+                                    )}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                    <input
+                        onChange={selectFiles}
+                        type="file"
+                        multiple
+                        name="file"
+                    />
+                    <button onClick={upload} type="submit">
+                        Submit
+                    </button>
+                </Tab>
+                <Tab name="Link Url">
+                    <input type="text" name="url" placeholder="URL" />
+                    <button type="submit">Link</button>
+                </Tab>
+            </Tabs>
+        </Card>
+    );
+};
