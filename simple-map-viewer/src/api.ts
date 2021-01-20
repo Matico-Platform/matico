a89971a6dcf534150919bc9c0e0c65ae5b045077
Stuart Lynn <stuart.lynn@gmail.com>
2021-01-20 12:44:29 -0500

    upload function in api

--- simple-map-viewer/src/api.ts
+++ simple-map-viewer/src/api.ts
@@ -1,18 +1,41 @@
-import axios from 'axios'
+import axios from 'axios';
 
 let a = axios.create({
     baseURL: 'http://localhost:8080',
-    headers: {"Content-Type":"application/json"}
+    headers: { 'Content-Type': 'application/json' },
 });
 
-a.interceptors.request.use((config)=>{
-    const token = localStorage.getItem("token");
-    if(token){
-        config.headers.Authorization= `Bearer ${token}`
-    }
-    return config
-},(error)=>{
-    return Promise.reject(error);
-});
+a.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    },
+);
+
+export function uploadFile(
+    file: File,
+    url: string,
+    onProgress: (progress: number) => void,
+) {
+    let formData = new FormData();
+    formData.append('file', file);
+
+    const progress = (e: any) => {
+        onProgress(Math.round((100 * e.loaded) / e.total));
+    };
+
+    return a.post(url, formData, {
+        headers: {
+            Content_Type: 'multipart/form-data',
+        },
+        onUploadProgress: progress,
+    });
+}
 
 export default a;
