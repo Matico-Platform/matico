22eb069ba869b0a7b0b3f2814b27cb47e0daa568
Stuart Lynn <stuart.lynn@gmail.com>
2021-01-27 11:05:00 -0500

    Tidyup and add static hosting to main

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 #[macro_use]
 extern crate diesel;
-
 extern crate argon2;
 
 use actix_cors::Cors;
-use actix_web::dev::ServiceRequest;
-use actix_web::middleware::Logger;
-use actix_web::{http, middleware, web, App, Error, HttpServer, Responder};
-use actix_web_httpauth::extractors::{
-    bearer::{BearerAuth, Config},
-    AuthenticationError,
-};
-use actix_web_httpauth::middleware::HttpAuthentication;
+use actix_files as fs;
+use actix_web::{middleware, web, App, HttpServer};
 use diesel::r2d2::{self, ConnectionManager};
+use std::path::PathBuf;
 
 mod app_config;
 mod auth;
@@ -24,33 +18,13 @@ mod schema;
 mod tiler;
 mod utils;
 
-async fn home() -> impl Responder {
-    format!("Hey buddy!")
+async fn home() -> std::io::Result<fs::NamedFile> {
+    let path: PathBuf = "./static/index.html".parse().unwrap();
+    Ok(fs::NamedFile::open(path)?)
 }
-
-// async fn validator(req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, Error> {
-//     return Ok(req);
-//     println!("VALIDATING");
-//     let config = req
-//         .app_data::<Config>()
-//         .map(|data| data.clone())
-//         .unwrap_or_else(Default::default);
-
-//     match auth::validate_token(credentials.token()) {
-//         Ok(res) => {
-//             if res == true {
-//                 Ok(req)
-//             } else {
-//                 Err(AuthenticationError::from(config).into())
-//             }
-//         }
-//         Err(_) => Err(AuthenticationError::from(config).into()),
-//     }
-// }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // std::env::set_var("RUST_BACKTRACE", "1");
     let config = app_config::Config::from_conf().unwrap();
     let manager = ConnectionManager::<diesel::pg::PgConnection>::new(config.db_string);
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
@@ -72,10 +46,11 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .wrap(middleware::Logger::new("%{Content-Type}i"))
             .route("/", web::get().to(home))
-            .service(web::scope("/tile").configure(tiler::init_routes))
+            .service(web::scope("/tiler").configure(tiler::init_routes))
             .service(web::scope("/users").configure(routes::users::init_routes))
             .service(web::scope("/auth").configure(routes::auth::init_routes))
             .service(web::scope("/datasets").configure(routes::datasets::init_routes))
+            .service(fs::Files::new("/", "static").show_files_listing())
     })
     .bind(config.server_addr.clone())?
     .run();
