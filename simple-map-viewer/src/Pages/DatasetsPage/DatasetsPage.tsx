a17b9f64d3e29729b89348af1c3e3e5e880766ee
Stuart Lynn <stuart.lynn@gmail.com>
2021-01-29 23:01:38 -0500

    Add dataset list to dataset page

--- simple-map-viewer/src/Pages/DatasetsPage/DatasetsPage.tsx
+++ simple-map-viewer/src/Pages/DatasetsPage/DatasetsPage.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { DatasetCreationForm } from '../../Components/DatasetCreationForm/DatasetCreationForm';
-import {DatasetList} from '../../Components/DatasetList/DatasetList'
-import {Styles} from './DatasetsPageStyles'
+import { DatasetList } from '../../Components/DatasetList/DatasetList';
+import { Styles } from './DatasetsPageStyles';
+import {
+    Page,
+    PageContent,
+    DetailsArea,
+} from '../../Components/Layout/Layout';
 
 interface DatasetPageProps {}
 
 export const DatasetsPage: React.FC<DatasetPageProps> = ({}) => {
     return (
-        <Styles.DatasetPage>
-            <DatasetList/>
-            <DatasetCreationForm />
-        </Styles.DatasetPage>
+        <Page>
+            <PageContent>
+                <DatasetList />
+                <DatasetCreationForm />
+            </PageContent>
+        </Page>
     );
 };
--- simple-map-viewer/src/Pages/DatasetsPage/DatasetsPageStyles.tsx
+++ simple-map-viewer/src/Pages/DatasetsPage/DatasetsPageStyles.tsx
@@ -1,13 +1,12 @@
-import styled from 'styled-components'
-
-const DatasetPage= styled.div`
+import styled from 'styled-components';
+const DatasetPage = styled.div`
     display: flex;
-    flex-direction:column;
-    justify-content:space-around;
-    align-items:flex-start;
-    flex:1;
-`
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: flex-start;
+    flex: 1;
+`;
 
-export const Styles={
-    DatasetPage
-}
+export const Styles = {
+    DatasetPage,
+};
