9d61fdffa18daf505cf201e446a03890a1385bc2
Stuart Lynn <stuart.lynn@gmail.com>
2021-01-31 21:55:39 -0500

    Adding Paper component to the layout components

--- simple-map-viewer/src/Components/Layout/Layout.tsx
+++ simple-map-viewer/src/Components/Layout/Layout.tsx
@@ -19,10 +19,14 @@ export const NavArea = styled.nav`
 export const Page = styled.div`
     grid-area: main;
     display: flex;
+    width:100%;
+    height:100%;
     flex-direction: row;
 `;
 export const PageContent = styled.div`
     flex: 1;
+    width:100%;
+    height:100%;
 `;
 
 export const DetailsArea = styled.div`
@@ -42,3 +46,39 @@ export const MainArea = styled.div`
     align-items: center;
     justify-content: center;
 `;
+
+export const Paper = styled.div`
+       position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
+    padding:10px;
+    border-radius:10px;
+    background-color:white;
+
+:before, :after
+{
+  content:"";
+    position:absolute;
+    z-index:-1;
+    -webkit-box-shadow:0 0 20px rgba(0,0,0,0.8);
+    -moz-box-shadow:0 0 20px rgba(0,0,0,0.8);
+    box-shadow:0 0 20px rgba(0,0,0,0.8);
+    top:10px;
+    bottom:10px;
+    left:0;
+    right:0;
+    -moz-border-radius:100px / 10px;
+    border-radius:100px / 10px;
+}
+:after
+{
+  right:10px;
+    left:auto;
+    -webkit-transform:skew(8deg) rotate(3deg);
+       -moz-transform:skew(8deg) rotate(3deg);
+        -ms-transform:skew(8deg) rotate(3deg);
+         -o-transform:skew(8deg) rotate(3deg);
+            transform:skew(8deg) rotate(3deg);
+}
+`
