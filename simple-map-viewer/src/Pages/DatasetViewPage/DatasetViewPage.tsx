9f8c5be53849bbf73be7a4343aead40eef039287
Stuart Lynn <stuart.lynn@gmail.com>
2021-04-06 09:37:56 -0400

    Code formatting for dataset view page

--- simple-map-viewer/src/Pages/DatasetViewPage/DatasetViewPage.tsx
+++ simple-map-viewer/src/Pages/DatasetViewPage/DatasetViewPage.tsx
@@ -10,7 +10,7 @@ import { DataTable } from 'Components/DataTable/DataTable';
 import { QueryPane } from 'Components/QueryPane/QueryPane';
 import { Tabs, Tab } from 'Components/Tabs/Tabs';
 import { FeatureEditor } from 'Components/FeatureEditor/FeatureEditor';
-import {Pagination} from 'Components/Pagination/Pagination'
+import { Pagination } from 'Components/Pagination/Pagination';
 
 // import * as d3 from 'd3';
 
@@ -41,15 +41,16 @@ const makeMvtLayer = (data: string | null, options = {}) => {
         return new MVTLayer({
             data: data,
             // @ts-ignore
+            getLineColor: [255, 0, 0, 255],
+            getLineWidth: 1,
+            lineWidthUnits: 'pixels',
             getFillColor: [226, 125, 96, 200],
-            getLineColor: [4, 4, 4],
             getBorderColor: [200, 200, 200],
-            getLineWidth: 10,
             getRadius: 40,
             stroked: true,
             pickable: true,
             autoHighlight: true,
-            highlightColor: [200,100,200,200],
+            highlightColor: [200, 100, 200, 200],
             radiusUnits: 'pixels',
             ...options,
         });
@@ -63,11 +64,11 @@ export const DatasetViewPage: React.FC = () => {
     const { id } = useParams<ParamTypes>();
     const [sqlString, setSQLQuery] = useState<any>(null); // this should go into useDataset
     const [selectedRow, setSelectedRow] = useState<any>(null); // this should go into useDataset
-    const [selectedTab, setSelectedTab] = useState<any>("Query")
+    const [selectedTab, setSelectedTab] = useState<any>('Query');
     const [hoverInfo, setHoverInfo] = useState<any>(null);
     const perPage = 100;
 
-    console.log("Hover info ", hoverInfo)
+    console.log('Hover info ', hoverInfo);
 
     const {
         dataset,
@@ -79,7 +80,14 @@ export const DatasetViewPage: React.FC = () => {
         ? { sql: sqlString }
         : { datasetId: id };
 
-    const { data, loading: dataLoading, error,pages, page,setPage} = useDataSource(dataQueryStrategy, {perPage});
+    const {
+        data,
+        loading: dataLoading,
+        error,
+        pages,
+        page,
+        setPage,
+    } = useDataSource(dataQueryStrategy, { perPage });
 
     if (datasetLoading) {
         return <div>LOADING...</div>;
@@ -95,14 +103,14 @@ export const DatasetViewPage: React.FC = () => {
         // updateFeature(id, update)
     };
 
-    const selectFeature = (feature: any)=>{
-        setSelectedRow(feature)
-        setSelectedTab("Feature")
-    }
-    const mapClicked = (info:any, event:any)=>{
-        console.log("MAP CLICK ",info, event)
-        selectFeature(info.object.properties)
-    }
+    const selectFeature = (feature: any) => {
+        setSelectedRow(feature);
+        setSelectedTab('Feature');
+    };
+    const mapClicked = (info: any, event: any) => {
+        console.log('MAP CLICK ', info, event);
+        selectFeature(info.object.properties);
+    };
 
     return (
         <Page>
@@ -121,27 +129,34 @@ export const DatasetViewPage: React.FC = () => {
                 {dataset && (
                     <Styles.Content>
                         <Styles.Table>
-                            {dataLoading  ?
+                            {dataLoading ? (
                                 <h3>Loading</h3>
-                            :
-                            <DataTable
-                                data={data}
-                                selectedID={selectedRow ? selectedRow[dataset.id_col] : null}
-                                onSelect={selectFeature}
-                                idCol = {dataset.id_col}
-                            />
-                            }
-
-                            {pages &&
-                            <Styles.TablePagination>
-                                <Pagination
-                                    pages={pages!}
-                                    page={page}
-                                    onPageChange={(page)=> setPage(page)}
+                            ) : (
+                                <DataTable
+                                    data={data}
+                                    selectedID={
+                                        selectedRow
+                                            ? selectedRow[
+                                                  dataset.id_col
+                                              ]
+                                            : null
+                                    }
+                                    onSelect={selectFeature}
+                                    idCol={dataset.id_col}
                                 />
-                            </Styles.TablePagination>
-                            }
-                        
+                            )}
+
+                            {pages && (
+                                <Styles.TablePagination>
+                                    <Pagination
+                                        pages={pages!}
+                                        page={page}
+                                        onPageChange={(page) =>
+                                            setPage(page)
+                                        }
+                                    />
+                                </Styles.TablePagination>
+                            )}
                         </Styles.Table>
                         <Styles.Map>
                             <DeckGL
@@ -152,18 +167,25 @@ export const DatasetViewPage: React.FC = () => {
                                 controller={true}
                                 onClick={mapClicked}
                                 onHover={(info: any) => {
-                                    setHoverInfo(info)
+                                    setHoverInfo(info);
                                 }}
                             >
                                 <StaticMap
                                     mapboxApiAccessToken={TOKEN}
                                     width={'100%'}
                                     height={'100%'}
-                                    mapStyle='mapbox://styles/mapbox/dark-v10'
+                                    mapStyle="mapbox://styles/mapbox/dark-v10"
                                 />
-                                {  (hoverInfo && hoverInfo.object) &&
-                                    <HoverToolTip x={hoverInfo.x} y={hoverInfo.y} info={hoverInfo.object.properties} />
-                                }
+                                {hoverInfo && hoverInfo.object && (
+                                    <HoverToolTip
+                                        x={hoverInfo.x}
+                                        y={hoverInfo.y}
+                                        info={
+                                            hoverInfo.object
+                                                .properties
+                                        }
+                                    />
+                                )}
                             </DeckGL>
                         </Styles.Map>
 
