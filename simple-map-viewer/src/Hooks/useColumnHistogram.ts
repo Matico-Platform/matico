4e2ae9fbaa4eb00260228696bbce81f4aea9c700
Stuart Lynn <stuart.lynn@gmail.com>
2021-04-06 09:29:04 -0400

    Hooks to get various stats from the backend

---
+++ simple-map-viewer/src/Hooks/useColumnHistogram.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import { Column, LayerSource, getColumnHistogram } from 'api';
+
+export const useColumnHistogram = (
+    column: Column,
+    source: LayerSource,
+    bins: number,
+) => {
+    const [data, setData] = useState<any>(null);
+
+    useEffect(() => {
+        if (column && source && bins) {
+            getColumnHistogram(column, source, bins)
+                .then((result) => {
+                    setData(result.data.Histogram);
+                })
+                .catch((e) => {
+                    console.log(
+                        'Something went wrong getting histogram ',
+                        e,
+                    );
+                    setData(undefined);
+                });
+        }
+    }, [column, source, bins]);
+    return data;
+};
