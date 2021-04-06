571b6347587e5fcfab3391435d2fab352c746e36
Stuart Lynn <stuart.lynn@gmail.com>
2021-04-06 09:28:39 -0400

    Removing extra surrounding element

--- simple-map-viewer/src/Components/LayerList/LayerList.tsx
+++ simple-map-viewer/src/Components/LayerList/LayerList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Styles } from './LayerListStyles';
-import { LayerControlls } from '../LayerControlls/LayerControlls';
+import { LayerControlls } from '../LayerControls/LayerControls';
 import { useDashboard } from '../../Contexts/DashbardBuilderContext';
 
 export const LayerList: React.FC = () => {
@@ -10,9 +10,7 @@ export const LayerList: React.FC = () => {
     return (
         <Styles.LayerList>
             {layers.map((layer) => (
-                <>
-                    <LayerControlls key={layer.name} layer={layer} />
-                </>
+                <LayerControlls key={layer.name} layer={layer} />
             ))}
         </Styles.LayerList>
     );
--- simple-map-viewer/src/Components/LayerList/LayerListStyles.tsx
+++ simple-map-viewer/src/Components/LayerList/LayerListStyles.tsx
@@ -1,11 +1,11 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
+const LayerList = styled.div`
+    padding: 10px;
+    flex: 1;
+    overflow-y: auto;
+`;
 
-const LayerList = styled.div` 
-    padding:10px;
-`
-
-
-export const Styles ={
-    LayerList
-}
+export const Styles = {
+    LayerList,
+};
