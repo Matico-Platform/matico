2cc5bd00548c0375e13e6593546659617ec1fd1a
Stuart Lynn <stuart.lynn@gmail.com>
2021-10-16 17:53:44 -0400

    Properly adding deck gl

--- matico_components/src/Components/Panes/MaticoMapPane/MaticoMapPane.tsx
+++ matico_components/src/Components/Panes/MaticoMapPane/MaticoMapPane.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { MapPane, LngLat } from "matico_spec";
 import type { MaticoPaneInterface } from "../Pane";
 import { StaticMap } from "react-map-gl";
+import DeckGL from "@deck.gl/react";
 import { Box } from "grommet";
 import ReactMapGL from "react-map-gl";
 
@@ -9,22 +10,32 @@ interface MaicoMapPaneInterface extends MaticoPaneInterface {
   inital_lng_lat: LngLat;
 }
 
-export const MaticoMapPane: React.FC<MaicoMapPaneInterface> = ({inital_lng_lat}) => {
+export const MaticoMapPane: React.FC<MaicoMapPaneInterface> = ({
+  inital_lng_lat,
+}) => {
   return (
     <Box fill={true}>
-      <StaticMap
-      zoom={7}
-      latitude={inital_lng_lat.lat}
-      longitude={inital_lng_lat.lng}
-        mapboxApiAccessToken={
-          "pk.eyJ1Ijoic3R1YXJ0LWx5bm4iLCJhIjoiY2t1dThkcG1xM3p2ZzJ3bXhlaHFtdThlYiJ9.rmndXXXrC5HAbxg1Ok8XTg"
-        }
-        mapStyle={
-          "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
-        }
+      <DeckGL
         width={"100%"}
         height={"100%"}
-      />
+        initialViewState={{
+          longitude: inital_lng_lat.lng,
+          latitude: inital_lng_lat.lat,
+          zoom: 7,
+        }}
+        controller={true}
+      >
+        <StaticMap
+          mapboxApiAccessToken={
+            "pk.eyJ1Ijoic3R1YXJ0LWx5bm4iLCJhIjoiY2t1dThkcG1xM3p2ZzJ3bXhlaHFtdThlYiJ9.rmndXXXrC5HAbxg1Ok8XTg"
+          }
+          mapStyle={
+            "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+          }
+          width={"100%"}
+          height={"100%"}
+        />
+      </DeckGL>
     </Box>
   );
 };
