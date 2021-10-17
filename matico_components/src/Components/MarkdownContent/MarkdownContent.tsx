4dcc46148adad0584ed578cae035c20648a18f3a
Stuart Lynn <stuart.lynn@gmail.com>
2021-10-17 18:18:30 -0400

    Adding ability to write math in markdown

--- matico_components/src/Components/MarkdownContent/MarkdownContent.tsx
+++ matico_components/src/Components/MarkdownContent/MarkdownContent.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
-import {Box} from 'grommet'
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import { Box } from "grommet";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import "katex/dist/katex.min.css"; // `rehype-katex` does not import the CSS for you
 
-interface MarkdownContentInferface{}
-export const MarkdownContnet: React.FC<MarkdownContentInferface> = ({children})=>{
-  // TODO this is dumb. should be an easier way of only selecting string children 
+interface MarkdownContentInferface {}
+export const MarkdownContnet: React.FC<MarkdownContentInferface> = ({
+  children,
+}) => {
+  // TODO this is dumb. should be an easier way of only selecting string children
   return (
     <Box>
-      <ReactMarkdown>
-        {typeof(children) === typeof('string') ? children as string : ''}
+      <ReactMarkdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+        {typeof children === typeof "string" ? (children as string) : ""}
       </ReactMarkdown>
     </Box>
-  )
-}
+  );
+};
