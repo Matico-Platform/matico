129f763f14e961062c62f19a681cd77d241db0c4
Stuart Lynn <stuart.lynn@gmail.com>
2021-11-23 12:55:44 -0500

    new variable spec for VarOrs

--- matico_spec/src/variables.rs
+++ matico_spec/src/variables.rs
@@ -7,12 +7,43 @@ pub struct Variable {
     bind: Option<bool> 
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone )]
+pub struct QuantileParams{
+    bins: u32
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone )]
+pub struct JenksParams{
+    bins: u32
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone )]
+pub enum DatasetMetric{
+   Min,
+   Max,
+   Quantile(QuantileParams),
+   Jenks(JenksParams),
+   Mean,
+   Median,
+   Summary
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Validate)]
+pub struct DatasetVal{
+    dataset: String,
+    column: Option<String>,
+    metric: Option<DatasetMetric>,
+    feature_id: Option<String>
+}
+
+
 #[derive(Serialize, Debug, Deserialize, Clone)]
 #[serde(untagged)]
 pub enum VarOr<T>
 {
     Var(Variable),
     Value(T),
+    DVal(DatasetVal)
 }
 
 impl<T> Validate for VarOr<T>
@@ -29,7 +60,8 @@ where
 
         match self {
             Self::Var(v) => ValidationErrors::merge(result, "Variable", v.validate()),
-            Self::Value(val) => ValidationErrors::merge(result, "ChartPane", val.validate()),
+            Self::DVal(v) => ValidationErrors::merge(result, "", v.validate()),
+            Self::Value(val) => ValidationErrors::merge(result, "Value", val.validate()),
         }
     }
 }
