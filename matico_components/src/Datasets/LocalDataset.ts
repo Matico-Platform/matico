b44d3745107bcda9eb01d4f6ccacc148dab1823d
Stuart Lynn <stuart.lynn@gmail.com>
2021-11-23 12:58:17 -0500

    Adding aggregation values to LocalDataset

--- matico_components/src/Datasets/LocalDataset.ts
+++ matico_components/src/Datasets/LocalDataset.ts
@@ -7,7 +7,7 @@ import {
   DatasetState,
 } from "./Dataset";
 
-import { predicate, DataFrame} from "@apache-arrow/es5-cjs";
+import { predicate, DataFrame } from "@apache-arrow/es5-cjs";
 
 export class LocalDataset implements Dataset {
   private _isReady: boolean;
@@ -46,18 +46,62 @@ export class LocalDataset implements Dataset {
     return this._columns;
   }
 
-  getFeature(feature_id: string){
-    const selectPredicate = predicate.col(this.idCol).eq(feature_id)   
-    const results = this._getResultsFromPredicate(selectPredicate)
-    return results
+  getFeature(feature_id: string) {
+    const selectPredicate = predicate.col(this.idCol).eq(feature_id);
+    const results = this._getResultsFromPredicate(selectPredicate);
+    return results;
   }
 
-  getArrow(){
-    return this._data.serialize() 
+  getArrow() {
+    return this._data.serialize();
   }
 
   getDataWithGeo(filters?: Array<Filter>) {
     return this.getData(filters);
+  }
+
+  _applyAggregateFunction(
+    column: string,
+    aggFunc: (agg: number, val: number) => number,
+    filters?: Array<Filter>
+  ) {
+    let base = null;
+    if (filters && filters.length) {
+      const predicate = this._constructPredicate(filters);
+      base = this._data.filter(predicate);
+    } else {
+      base = this._data;
+    }
+    let columnVals = [];
+    let agg: number | null = null;
+    base.scan(
+      (index) => {
+        agg = aggFunc(agg, columnVals[index]);
+      },
+      (batch) => {
+        //@ts-ignore
+        columnVals = predicate.col(column).bind(batch);
+      }
+    );
+    return agg;
+  }
+
+  getColumnMax(column: string, filters?: Array<Filter>) {
+    return this._applyAggregateFunction(column, (agg,val)=>
+      val > agg ? val : agg
+    , filters)
+  }
+
+  getColumnMin(column: string, filters?: Array<Filter>) {
+    return this._applyAggregateFunction(column, (agg,val)=>
+      val < agg ? val : agg
+    , filters)
+  }
+
+  getColumnSum(column: string, filters?: Array<Filter>) {
+    return this._applyAggregateFunction(column, (agg,val)=>
+      agg+=val 
+    , filters)
   }
 
   _constructPredicate(filters?: Array<Filter>) {
@@ -92,52 +136,53 @@ export class LocalDataset implements Dataset {
     return combinedPredicate;
   }
 
-  _getResultsFromPredicate(filterPredicate: any, columns?: Array<string>){
-      const filterResults = this._data.filter(filterPredicate);
+  _getResultsFromPredicate(filterPredicate: any, columns?: Array<string>) {
+    const filterResults = this._data.filter(filterPredicate);
 
-      try{
-        const vars = {};
-        let results = [];
+    try {
+      const vars = {};
+      let results = [];
 
-        let selectColumns = columns ? columns : this.columns().map((c) => c.name);
+      let selectColumns = columns ? columns : this.columns().map((c) => c.name);
 
-        selectColumns = [...selectColumns, "geom"];
+      selectColumns = [...selectColumns, "geom"];
 
-        filterResults.scan(
-          (index) => {
-            results.push(
-              selectColumns.reduce((agg, col) => {
-                return {
-                  ...agg,
-                  //@ts-ignore
-                  [col]: vars[col](index),
-                };
-              },
-              {}
-            ))
-          },
-          (batch) => {
-            selectColumns.forEach((col) => {
-              vars[col] = predicate.col(col).bind(batch);
-            });
-          }
-        );
-      return results
-      }
-      catch{
-        console.warn("something went wrong applying filters cowardly returning empty array")
-        return []
-      }
+      filterResults.scan(
+        (index) => {
+          results.push(
+            selectColumns.reduce((agg, col) => {
+              return {
+                ...agg,
+                //@ts-ignore
+                [col]: vars[col](index),
+              };
+            }, {})
+          );
+        },
+        (batch) => {
+          selectColumns.forEach((col) => {
+            vars[col] = predicate.col(col).bind(batch);
+          });
+        }
+      );
+      return results;
+    } catch {
+      console.warn(
+        "something went wrong applying filters cowardly returning empty array"
+      );
+      return [];
+    }
   }
 
   getData(filters?: Array<Filter>, columns?: Array<string>) {
+    console.log("filters are ", filters);
     const cacheKey = JSON.stringify([filters, columns]);
     if (this._filterCache[cacheKey]) {
       return this._filterCache[cacheKey];
     }
     if (filters && filters.length) {
       const predicate = this._constructPredicate(filters);
-      const results  = this._getResultsFromPredicate(predicate)
+      const results = this._getResultsFromPredicate(predicate);
 
       this._filterCache[JSON.stringify(filters)] = results;
       return results;
