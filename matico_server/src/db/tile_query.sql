ead9a59627c87937cba00e57852f747d19d927a8
Stuart Lynn <stuart.lynn@gmail.com>
2022-01-31 17:27:34 -0500

    Better tile query that seems to work better

--- matico_server/src/db/tile_query.sql
+++ matico_server/src/db/tile_query.sql
@@ -4,15 +4,12 @@ SELECT ST_AsMVT(q, 'layer', 4096, 'mvt_geom') as mvt
           {columns},
         --   ogc_fid::INTEGER as id,
           ST_AsMVTGeom(
-              geom,
-              ST_TRANSFORM(ST_MakeEnvelope({x_min}, {y_min}, {x_max}, {y_max}, 3857), 4326),
-              4096,
-              256,
-              true
+              ST_TRANSFORM(geom, 3857),
+              ST_TileEnvelope({z},{x},{y})
           ) mvt_geom
       FROM (
         select *, {geom_column} as geom from ({tile_table}) b
-        where {geom_column} && ST_TRANSFORM(ST_MakeEnvelope({x_min}, {y_min}, {x_max}, {y_max}, 3857), 4326)
+        where ST_INTERSECTS({geom_column},ST_TRANSFORM(ST_TileEnvelope({z},{x},{y}), 4326))
       ) c
     ) q
     limit 1 
