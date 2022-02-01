2695a6c4a686eb1a3b4cf08c3ce7f0b7b20cdb37
Stuart Lynn <stuart.lynn@gmail.com>
2022-02-01 14:04:43 -0500

    Working parameters for apis

--- matico_admin/components/VariableEditor.tsx
+++ matico_admin/components/VariableEditor.tsx
@@ -5,38 +5,109 @@ import {
   IllustratedMessage,
   Item,
   ListBox,
+  TextField,
+  Picker,
   View,
+  Text,
+  ActionButton,
+  NumberField,
 } from "@adobe/react-spectrum";
 import NotFound from "@spectrum-icons/illustrations/NotFound";
-import React from "react";
+import React, { Key } from "react";
 
 interface VariableEditorProps {
   parameters: Array<any>;
+  onParametersChanged: (newParams: Array<any>) => void;
 }
 
-export const VariableEditor: React.FC<VariableEditorProps> = ({ parameters}) => {
+export const VariableEditor: React.FC<VariableEditorProps> = ({
+  parameters,
+  onParametersChanged,
+}) => {
+
+  const updateParameterValue = (
+    name: string,
+    change: { [value: string]: any }
+  ) => {
+    const newParams = parameters.map((p) => 
+        p.name === name ? { ...p, ...change } : p
+    )
+    onParametersChanged(
+      newParams
+    );
+  };
+
+  const removeVar= (name:string)=>{
+    onParametersChanged(
+      parameters.filter(p=>p.name!==name)
+    )
+  }
+
   return (
     <Flex
       justifyContent="center"
       alignItems="center"
       width={"100%"}
       height={"100%"}
+      direction={"column"}
     >
-      {parameters?.length>0 ?
+      {!parameters || parameters?.length === 0 ? (
         <IllustratedMessage>
           <NotFound />
           <Content>Add variables here or add them to your query</Content>
         </IllustratedMessage>
-        :
+      ) : (
         <>
           <Heading>Variables</Heading>
-          <ListBox>
-            {parameters.map((variable: any) => (
-              <Item key={variable.name}>{variable.name}</Item>
+          <Flex flex={1} direction="column">
+            {parameters.map((p) => (
+              <Flex
+                key={p.name}
+                direction="row"
+                justifyContent="space-between"
+                gap={"size-200"}
+                alignItems="end"
+              >
+                <Text>{p.name}</Text>
+                <Picker
+                  label="Variable Type"
+                  selectedKey={p.type as Key}
+                  width={"size-1600"}
+                  onSelectionChange={(type) =>
+                    updateParameterValue(p.name, { type: type as string })
+                  }
+                >
+                  <Item key="Numerical">Numerical</Item>
+                  <Item key="Text">Text</Item>
+                </Picker>
+                <NumberField
+                  label="Default Value"
+                  value={Object.values(p.default_value)[0] as number}
+                  onChange={(default_value) =>
+                    updateParameterValue(p.name, { default_value: {Numeric:default_value}})
+                  }
+                  step={0.01}
+                  width={"size-900"}
+                />
+                <NumberField
+                  label="Value"
+                  value={p.value ?? Object.values(p.default_value)[0]}
+                  onChange={(value) =>
+                    updateParameterValue(p.name, { value})
+                  }
+                  formatOptions={{
+                    minimumFractionDigits:3
+                  }}
+                  step={0.01}
+
+                  width={"size-900"}
+                />
+                  <ActionButton onPress={()=>removeVar(p.name)}>Delete</ActionButton>
+              </Flex>
             ))}
-          </ListBox>
+          </Flex>
         </>
-      }
+      )}
     </Flex>
   );
 };
--- matico_admin/pages/apis/[apiID].tsx
+++ matico_admin/pages/apis/[apiID].tsx
@@ -15,6 +15,7 @@ import {
   TableView,
   View,
   Text,
+  Well,
 } from "@adobe/react-spectrum";
 import { GetServerSideProps } from "next";
 import { useApi, useApiTableData } from "../../hooks/useApis";
@@ -24,7 +25,7 @@ import { VariableEditor } from "../../components/VariableEditor";
 import { MapView } from "../../components/MapView";
 import { useMemo, useEffect, useState, memo } from "react";
 
-const parameterRegEx = /\{\{([A-Za-z0-9]*)\}\}/g;
+const parameterRegEx = /\$\{([A-Za-z0-9]*)\}/g;
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   return { props: { apiId: query.apiID } };
@@ -48,15 +49,24 @@ const Api: NextPage<{ apiId: string }> = ({ apiId }) => {
   const [lastRunKey, setLastRunKey] = useState(new Date().toISOString());
   const [localParameters, setLocalParameters] = useState<any[]>([]);
 
+
+  const valuesForQuery = localParameters.reduce((agg,val)=>{
+    return {...agg, [val.name]: val.value ?? Object.values(val.default_value)[0] }
+  },{})
+
   const {
     data: tableData,
     error,
     mutate: updateTableData,
-  } = useApiTableData(api, { params: { limit: 15 }, refreshInterval: 0 });
+  } = useApiTableData(api, { params: { limit: 15, ...valuesForQuery }, refreshInterval: 0 });
+
+
+  useEffect(()=>{
+      updateTableData()
+  },[JSON.stringify(valuesForQuery)])
 
 
   const Table = useMemo(() => {
-    console.log("RUNNING MEMO ");
     const columns = 
       tableData
         ? Object.keys(tableData[0]).map((item) => ({ id: item, name: item }))
@@ -87,9 +97,10 @@ const Api: NextPage<{ apiId: string }> = ({ apiId }) => {
     );
   }, [tableData]);
 
-  console.log("Api query is ", api?.sql.replace(/\n/g," "))
+  const urlParams: string = Object.keys(valuesForQuery).map(key=>`${encodeURIComponent(key)}=${encodeURIComponent(valuesForQuery[key])}`).join("&")
+  
   const mapUrl = api
-    ? `http://localhost:8000/api/tiler/api/${api.id}/{z}/{x}/{y}`
+    ? `http://localhost:8000/api/tiler/api/${api.id}/{z}/{x}/{y}?${urlParams}`
     : "";
 
   useEffect(() => {
@@ -101,21 +112,23 @@ const Api: NextPage<{ apiId: string }> = ({ apiId }) => {
 
   useEffect(()=>{
     const variables = parseVariables(query)
+    const newVariables: Array<any> = []
     variables.forEach((variable)=>{
       if(!localParameters.find(p=>p.name === variable)){
-        localParameters.push({
+        newVariables.push({
           type:'Numerical',
           name:variable,
           description:`The value of ${variable}`,
-          default_value:0
+          default_value:{Numeric:0}
         })
       }
     })
+    setLocalParameters([...localParameters,...newVariables])
   },[query])
 
   const updateAndRun = () => {
     setLastRunKey(new Date().toISOString());
-    updateApi({ sql: query.replace(";", "") });
+    updateApi({ sql: query.replace(";", ""), parameters:localParameters });
     setTimeout(() => {
       updateTableData();
     });
@@ -127,6 +140,7 @@ const Api: NextPage<{ apiId: string }> = ({ apiId }) => {
     });
   };
 
+
   return (
     <Layout hasSidebar={true}>
       <View
@@ -137,6 +151,15 @@ const Api: NextPage<{ apiId: string }> = ({ apiId }) => {
       >
         <Heading level={2}>{api ? api.name : "Loading..."}</Heading>
         <Content>{api && api.description}</Content>
+        <Heading level={4}>Api URLS</Heading>
+        <Well>
+          <Heading>Data endpoint</Heading>
+          <p>{`http://localhost:8000/api/apis/${api?.id}&${urlParams}`}</p>
+        </Well>
+        <Well>
+          <Heading>Tile endpoint</Heading>
+          <p>{mapUrl}</p>
+        </Well>
       </View>
       <View gridArea="content" padding="size-200" width="100%" height="95%">
         {api && (
@@ -155,7 +178,10 @@ const Api: NextPage<{ apiId: string }> = ({ apiId }) => {
                 onQueryChange={setQuery}
               />
               <View width={"25%"} height={"100%"}>
-                <VariableEditor parameters={[]} />
+                <VariableEditor 
+                  parameters={localParameters}
+                  onParametersChanged={(newParams)=> setLocalParameters(newParams)}
+                />
               </View>
             </Flex>
             <Flex gridArea="buttons" gap={"10px"} justifyContent={"end"}>
