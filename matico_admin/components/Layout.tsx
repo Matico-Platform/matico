eb01d886ba218e17b986d74da791002bbf6d724c
Dylan <dhalpern@uchicago.edu>
2022-02-24 16:59:41 -0600

    Admin layout fixes

--- matico_admin/components/Layout.tsx
+++ matico_admin/components/Layout.tsx
@@ -26,15 +26,32 @@ if (typeof window !== 'undefined') {
 
 export const Layout: React.FC<LayoutProps> = ({ children, hasSidebar }) => {
 
-  const areas = hasSidebar ? ["header  header", "sidebar content", "footer  footer"]
-                           : ["header  header", "content content", "footer  footer"]
+  const areas = {
+    L: ["header  header", `${hasSidebar ? 'sidebar' : 'content'} content`, "footer  footer"],
+    M: ["header  header", `${hasSidebar ? 'sidebar' : 'content'} content`, "footer  footer"],
+    S: ["header  header", ...(hasSidebar ? ['sidebar sidebar', 'content content'] : ['content content']), "footer  footer"],
+    base: ["header  header", ...(hasSidebar ? ['sidebar sidebar', 'content content'] : ['content content']), "footer  footer"]
+  }
+
+  const columns = {
+    L: ["static-size-4600", "calc(100% - static-size-4600)"],
+    M: ["static-size-3600", "calc(100% - static-size-3600)"],
+    S: ["static-size-2600", "calc(100% - static-size-2600)"],
+    base: ["static-size-2000", "calc(100% - static-size-2000)"]
+  }
+
+  const rows = {
+    L: ["size-500", "auto", "size-300"],
+    M: ["size-500", "auto", "size-300"],
+    S: ["size-500", "size-2000", "auto", "size-300"],
+    base: ["size-500", "size-2000", "auto", "size-300"]
+  }
+
   return (
     <SSRProvider>
       <Provider theme={defaultTheme}>
         <Grid
-          areas={areas}
-          columns={["static-size-4600", "3fr"]}
-          rows={["size-500", "auto", "size-300"]}
+          {...{ areas, columns, rows }}
           height="100vh"
           justifyContent="stretch"
           width="100vw"
