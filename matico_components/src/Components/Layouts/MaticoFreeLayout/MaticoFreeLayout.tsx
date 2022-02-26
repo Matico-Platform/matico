d45cced581c36ec29e75cd21829ff21d55f52c03
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-02-26 13:49:39 -0600

    Swap Box for View

--- matico_components/src/Components/Layouts/MaticoFreeLayout/MaticoFreeLayout.tsx
+++ matico_components/src/Components/Layouts/MaticoFreeLayout/MaticoFreeLayout.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { Box } from 'grommet'
 import { PanePosition } from '@maticoapp/matico_spec'
 import Draggable from "react-draggable";
+import { View } from '@adobe/react-spectrum';
 
 const FreeArea = styled.div`
   position: relative;
@@ -13,11 +14,11 @@ const FreeArea = styled.div`
 
 const FreePane = styled(Box) <{ pane: PanePosition }>`
   position: absolute;
-  width:${({ pane }) => `${pane.width}${pane.width_units === "Percent" ? '%' : 'px' }`};
-  height:${({ pane }) => `${pane.height}${pane.height_units ==="Percent" ? '%' : 'px' }`};
+  width:${({ pane }) => `${pane.width}${pane.width_units === "Percent" ? '%' : 'px'}`};
+  height:${({ pane }) => `${pane.height}${pane.height_units === "Percent" ? '%' : 'px'}`};
   z-index:${({ pane }) => `${pane.layer}`};
-  left:${({ pane }) => `${pane.x}${pane.x_units ==="Percent" ? '%' : 'px' }`};
-  bottom:${({ pane }) => `${pane.y}${pane.y_units ==="Percent" ? '%' : 'px' }`};
+  left:${({ pane }) => `${pane.x}${pane.x_units === "Percent" ? '%' : 'px'}`};
+  bottom:${({ pane }) => `${pane.y}${pane.y_units === "Percent" ? '%' : 'px'}`};
   cursor:${({ pane }) => pane.float ? 'grab' : 'pointer'};
   background:${({ pane }) => pane.background ? pane.background : 'white'};
   transition: bottom 250ms, left 250ms, width 250ms, height 250ms, background 250ms;
@@ -31,10 +32,26 @@ export const MaticoFreeLayout: React.FC<MaticoFreeLayoutInterface> = ({ children
   return <FreeArea>
     {React.Children.map(children, child => {
       //@ts-ignore
+      const positionProps = child.props.position;
+      //@ts-ignore
       //TODO Make this properly typed. Properly check to ensure that the child nodes implement MaticoPaneInterface 
-      const pane = (<FreePane elevation={'medium'} key={child.props.name} pane={child.props.position} className="FreePane" >
+      const pane = (<View
+        key={child.props.name} 
+        // pane={child.props.position} 
+        // className="FreePane" 
+        position="absolute"
+        width={`${positionProps.width}${positionProps.width_units === "Percent" ? '%' : 'px'}`}
+        height={`${positionProps.height}${positionProps.height_units === "Percent" ? '%' : 'px'}`}
+        zIndex={positionProps.layer}
+        left={`${positionProps.x}${positionProps.x_units === "Percent" ? '%' : 'px'}`}
+        bottom={`${positionProps.y}${positionProps.y_units === "Percent" ? '%' : 'px'}`}
+        UNSAFE_style={{
+          transition: 'bottom 250ms, left 250ms, width 250ms, height 250ms, background 250ms',
+          boxShadow: '0px 10px 15px -3px rgba(0,0,0,0.1),3px -7px 15px -3px rgba(0,0,0,0.05)'
+        }}
+        >
         {child}
-      </FreePane>)
+      </View>)
 
       //@ts-ignore
       return child.props.float ? <Draggable>{pane}</Draggable> : pane
--- matico_components/src/Components/Panes/MaticoControlsPane/MaticoControlsPane.tsx
+++ matico_components/src/Components/Panes/MaticoControlsPane/MaticoControlsPane.tsx
@@ -3,9 +3,9 @@ import { MaticoPaneInterface } from "../Pane";
 import { MaticoRangeControl } from "./MaticoRangeControl";
 import { MaticoSelectControl } from "./MaticoSelectControl";
 import { useNormalizeSpec } from "../../../Hooks/useNormalizeSpec";
-import { View,Flex, Heading, Provider, lightTheme } from "@adobe/react-spectrum";
-import {useIsEditable} from "Hooks/useIsEditable";
-import {EditButton} from "../../../../dist/Components/MaticoEditor/Utils/EditButton";
+import { View, Flex, Heading, Provider, lightTheme } from "@adobe/react-spectrum";
+import { useIsEditable } from "Hooks/useIsEditable";
+import { EditButton } from "../../MaticoEditor/Utils/EditButton";
 
 export interface MaticoControlsPaneInterface extends MaticoPaneInterface {
   controls: Array<any>;
@@ -24,28 +24,30 @@ export const MaticoControlsPane: React.FC<MaticoControlsPaneInterface> = ({
   const edit = useIsEditable();
 
   return (
-    <Flex direction="column" margin={"size-200"} alignItems='stretch'>
-      <View UNSAFE_style={{position:"absolute", "top":"-20px", "left":"-20px"}}> 
+    <Flex direction="column" alignItems='stretch'>
+      <View position="absolute" top="-20px" left="-20px">
         <EditButton editPath={`${editPath}.Controls`} editType={"Controls"} />
       </View>
       <Provider theme={lightTheme}>
+        <View padding="size-200">
         <Heading>{title}</Heading>
-        <Flex direction="column" gap="size-200">
-        {mappedControls.map((controlSpec) => {
-          const [type, params] = Object.entries(controlSpec)[0];
-          //@ts-ignore
-          const { name } = params;
-          switch (type) {
-            case "Range":
-              return <MaticoRangeControl {...params} />;
-            case "Select":
+          <Flex direction="column" gap="size-200">
+            {mappedControls.map((controlSpec) => {
+              const [type, params] = Object.entries(controlSpec)[0];
               //@ts-ignore
-              return <MaticoSelectControl {...params} />;
-            default:
-              throw Error(`Unsupported fitler type ${type}`);
-          }
-        })}
-        </Flex>
+              const { name } = params;
+              switch (type) {
+                case "Range":
+                  return <MaticoRangeControl {...params} />;
+                case "Select":
+                  //@ts-ignore
+                  return <MaticoSelectControl {...params} />;
+                default:
+                  throw Error(`Unsupported fitler type ${type}`);
+              }
+            })}
+          </Flex>          
+        </View>
       </Provider>
     </Flex>
   );
