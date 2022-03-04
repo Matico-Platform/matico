389e904cbd8a28f84a2e659793ee9fb07af33a7d
Dylan <dhalpern@uchicago.edu>
2022-03-03 23:08:45 -0600

    State Viewer background

--- matico_components/src/Components/MaticoEditor/Panes/MaticoStateViewer.tsx
+++ matico_components/src/Components/MaticoEditor/Panes/MaticoStateViewer.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactJson from "react-json-view";
 import { useMaticoSelector } from "Hooks/redux";
-import { Flex } from "@adobe/react-spectrum";
+import { Flex, View } from "@adobe/react-spectrum";
 
 export const MaticoStateViewer: React.FC = () => {
   const state = useMaticoSelector((state) => state.variables);
   return (
     <Flex>
-      <ReactJson
-        style={{ fontSize: 15 }}
-        collapsed={3}
-        src={state}
-      />
+      <View backgroundColor="static-white" paddingX="medium" paddingY="medium">
+        <ReactJson
+          style={{ fontSize: 15, textAlign: "left" }}
+          collapsed={3}
+          src={state}
+
+        />
+      </View>
     </Flex>
   );
 };
