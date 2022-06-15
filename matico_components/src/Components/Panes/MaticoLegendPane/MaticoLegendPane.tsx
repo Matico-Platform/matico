6bb61c7fe67054e2127720b0b77c2949f8b1105d
Dylan <dhalpern@uchicago.edu>
2022-06-15 17:41:30 -0500

    WIP Legend

--- matico_components/src/Components/Panes/MaticoLegendPane/MaticoLegendPane.tsx
+++ matico_components/src/Components/Panes/MaticoLegendPane/MaticoLegendPane.tsx
@@ -1,6 +1,21 @@
 import { Flex, Heading, Text, View } from "@adobe/react-spectrum";
 import React from "react";
 import { generateColor, getColorScale } from "../MaticoMapPane/LayerUtils";
+import { scaleLinear, scaleOrdinal, scaleThreshold, scaleQuantile } from '@visx/scale';
+import {
+  Legend,
+  LegendLinear,
+  LegendQuantile,
+  LegendOrdinal,
+  LegendSize,
+  LegendThreshold,
+  LegendItem,
+  LegendLabel,
+} from '@visx/legend';
+import chroma from "chroma-js";
+import { colors } from "Utils/colors";
+import _ from "lodash";
+import prettyNum, {PRECISION_SETTING} from 'pretty-num';
 
 // interface MaicoMapPaneInterface extends MaticoPaneInterface {
 //     view: View;
@@ -12,8 +27,71 @@ import { generateColor, getColorScale } from "../MaticoMapPane/LayerUtils";
 
 
 
-export const MaticoLegendPane = ({ layers = [] }) => {
+export function nicelyFormatNumber(x: number | string){
+  console.log(x)
+  const val = +x;
+  if (!x || isNaN(val)) return x;
+  if (val < 0.0001) return val.toExponential();
+  if (val < 0.01) return val.toFixed(4);
+  if (val < 1) return val.toFixed(3);
+  if (val < 10) return val.toFixed(2);
+  if (val < 100) return val.toFixed(1);
+  if (val < 1_000) return val.toFixed(0);
+  if (val < 10_000) return `${(val/1_000).toFixed(1)}K`;
+  if (val < 1_000_000) return `${(val/1_000).toFixed(0)}K`;
+  if (val < 1_000_000_000) return `${(val/1_000_000).toFixed(1)}M`;
+  if (val < 1_000_000_000_000) return `${(val/1_000_000).toFixed(1)}B`;
+  return val.toExponential();
+}
 
+const Legend: React.FC<{layer: any}> = ({
+  layer={}
+}) => {
+  const {
+    name,
+    colorScale
+  } = layer
+
+  const {
+    range: colorScaleName,
+    domain
+  } = colorScale;
+
+  const range = _.get(colors, colorScaleName)
+  // const range = chroma.scale(colorScale).domain(domain?.length || 0);
+  const scale = scaleThreshold<number>({
+    domain,
+    range,
+  });
+
+  return <div>
+    <div>
+      {name}
+    </div>
+    <LegendThreshold scale={scale} labelFormat={nicelyFormatNumber}>
+      {(labels) => {
+        return labels.reverse().map((label, i) => (
+          <LegendItem
+            key={`legend-quantile-${i}`}
+            margin="1px 0"
+            // onClick={() => {
+            //   if (events) alert(`clicked: ${JSON.stringify(label)}`);
+            // }}
+          >
+            <svg width={20} height={20}>
+              <circle fill={`rgb(${label.value.join(',')})`} r={8} cx={10} cy={10} />
+            </svg>
+            <LegendLabel align="left" margin="2px 0 0 10px">
+              {label.text}
+            </LegendLabel>
+          </LegendItem>
+        ))}
+      }
+    </LegendThreshold>
+  </div> 
+}
+
+export const MaticoLegendPane: React.FC<{layers: any[]}> = ({ layers = [] }) => {
   return layers && layers.length ? (
     <View
       position="absolute"
@@ -24,64 +102,7 @@ export const MaticoLegendPane = ({ layers = [] }) => {
     >
       <Flex direction="row">
         {layers.map((layer) =>
-          layer?.colorScale?.domain && layer?.colorScale?.range ? (
-            <View key={layer.name}>
-              <Text>{layer.name}</Text>
-              <Flex direction="row">
-                <Flex direction="column-reverse">
-                  {"string" == typeof layer.colorScale.range
-                    ? getColorScale(layer.colorScale.range)[0]
-                      ? getColorScale(layer.colorScale.range)[0].map(
-                          (color) => (
-                            <View
-                              width="size-150"
-                              flexGrow={1}
-                              key={color}
-                              UNSAFE_style={{ backgroundColor: color }}
-                            />
-                          )
-                        )
-                      : null
-                    : layer?.colorScale?.range
-                    ? layer.colorScale.range.map((d) => {
-                        return (
-                          <View
-                            key={`rgb(${generateColor(d, false)
-                              .slice(0, 3)
-                              .join(",")})`}
-                            UNSAFE_style={{
-                              backgroundColor: `rgb(${generateColor(d, false)
-                                .slice(0, 3)
-                                .join(",")})`,
-                            }}
-                          />
-                        );
-                      })
-                    : null}
-                </Flex>
-                <Flex
-                  direction="column-reverse"
-                  justifyContent="space-evenly"
-                  alignItems="start"
-                  marginY={`${100 / layer.colorScale.range.length / 2}%`}
-                  marginStart="size-50"
-                >
-                  {!!layer?.colorScale?.domain &&
-                    layer.colorScale.domain
-                      .map((d) => (
-                        "string" === typeof(d) ? 
-                        <Text key={d}>
-                          {d}
-                        </Text>
-                        :
-                        <Text key={Math.round(d).toLocaleString("en")}>
-                          {(Math.round(d * 10) / 10).toLocaleString("en")}
-                        </Text>
-                      ))}
-                </Flex>
-              </Flex>
-            </View>
-          ) : null
+          <Legend layer={layer} />
         )}
       </Flex>
     </View>
