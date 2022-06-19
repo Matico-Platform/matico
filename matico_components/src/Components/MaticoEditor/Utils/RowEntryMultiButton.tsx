bd0e908282b91eb7beaf6e585a035636fcef4816
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-06-19 18:09:43 -0500

    Row Entry Multi button Cleanup

--- matico_components/src/Components/MaticoEditor/Utils/RowEntryMultiButton.tsx
+++ matico_components/src/Components/MaticoEditor/Utils/RowEntryMultiButton.tsx
@@ -11,7 +11,9 @@ import {
     ActionGroup,
     Item,
     Text,
-    Well
+    Well,
+    Divider,
+    DialogContainer
 } from "@adobe/react-spectrum";
 import Edit from "@spectrum-icons/workflow/Edit";
 import ChevronUp from "@spectrum-icons/workflow/ChevronUp";
@@ -42,117 +44,109 @@ export const RowEntryMultiButton: React.FC<RowEntryMultiButtonProps> = ({
         editPath,
         editType
     );
+    const [confirmDelete, setConfirmDelete] = React.useState(false);
 
     return (
-        <View
-            // UNSAFE_style={{
-            //   padding: compact ? ".25em 1em" : "inherit",
-            // }}
-            paddingX="size-150"
-            paddingY={compact ? "size-50" : "size-100"}
-            marginTop="size-100"
-            marginStart={`${inset}em`}
-            width={`calc(100% - ${inset}em)`}
-            backgroundColor="gray-100"
-            borderColor="gray-400"
-            borderWidth="thin"
-            borderRadius="medium"
+        <Flex
+            width={`100%`}
+            direction="row"
+            gap="size-50"
+            position={"relative"}
         >
-            <Flex
-                direction="row"
-                margin="0"
-                gap="size-50"
-                width="100%"
-                alignContent={"center"}
-                justifyContent={"space-between"}
+            {inset > 0 && <Divider
+                orientation="vertical"
+                position="absolute"
+                left={`${inset}em`}
+                top={0}
+                size="M"
+                height="100%"
+                UNSAFE_style={{
+                    pointerEvents: "none"
+                }}
+            />}
+            <Button
+                onPress={() => openEditor()}
+                variant="primary"
+                isQuiet
+                flex="1 1 auto"
+                maxWidth={"calc(100% - 80px)"}
+                UNSAFE_style={{
+                    paddingLeft: `calc(${inset}em + 6px)`,
+                    borderRadius: 0,
+                    cursor: "pointer"
+                }}
             >
-                <View
-                    maxWidth={"50%"}
-                    overflow={"hidden"}
-                    flexGrow={1}
-                    justifySelf={"left"}
-                    alignSelf="center"
-                    UNSAFE_style={{textAlign: 'left'}}
-                >
-                    <Text justifySelf="left">{entryName}</Text>
-                </View>
-                <ActionGroup
-                    isQuiet
-                    buttonLabelBehavior="hide"
-                    overflowMode="collapse"
-                    justifySelf={"end"}
-                    maxWidth="50%"
-                    marginStart={`-${inset/2}em`}
-                    onAction={(action) => {
-                        switch (action) {
-                            case "delete":
-                                remove();
-                                break;
-                            case "edit":
-                                openEditor();
-                                break;
-                            case "duplicate":
-                                duplicate();
-                                break;
-                            case "moveUp":
-                                reorder("forward");
-                                break;
-                            case "moveDown":
-                                reorder("backward");
-                                break;
-                            default:
-                                return;
-                        }
+                <Text
+                    justifySelf={"flex-start"}
+                    UNSAFE_style={{
+                        textAlign: "left",
                     }}
                 >
-                    <Item key="edit">
-                        <Settings />
-                        <Text>Edit</Text>
-                    </Item>
-
-                    <Item key="duplicate">
-                        <Duplicate />
-                        <Text>Duplicate</Text>
-                    </Item>
-                    <Item key="moveUp">
-                        <ChevronUp />
-                        <Text>Bring Forward</Text>
-                    </Item>
-                    <Item key="moveDown">
-                        <ChevronDown />
-                        <Text>Send Backward</Text>
-                    </Item>
-                </ActionGroup>
-
-                <View justifySelf={"end"}>
-                    <DialogTrigger
-                        isDismissable
-                        type="popover"
-                        mobileType="tray"
-                        containerPadding={1}
-                    >
-                        <ActionButton isQuiet>
-                            <Delete />
-                        </ActionButton>
-                        {(close) => (
-                            <Dialog>
-                                <Heading>Gone, forever?</Heading>
-                                <Content marginTop="size-100">
-                                    <Button
-                                        variant="negative"
-                                        onPress={() => {
-                                            remove();
-                                            close();
-                                        }}
-                                    >
-                                        <Delete /> Delete
-                                    </Button>
-                                </Content>
-                            </Dialog>
-                        )}
-                    </DialogTrigger>
-                </View>
-            </Flex>
-        </View>
+                    {entryName}
+                </Text>
+            </Button>
+            <DialogTrigger
+                isDismissable
+                type="popover"
+                mobileType="tray"
+                containerPadding={1}
+            >
+                <ActionButton isQuiet>
+                    <Delete />
+                </ActionButton>
+                {(close) => (
+                    <Dialog>
+                        <Heading>Are you sure you want to delete {entryName}?</Heading>
+                        <Content marginTop="size-100">
+                            <Button
+                                variant="negative"
+                                onPress={() => {
+                                    remove();
+                                    close();
+                                }}
+                            >
+                                <Delete /> Delete
+                            </Button>
+                        </Content>
+                    </Dialog>
+                )}
+            </DialogTrigger>
+            <ActionGroup
+                isQuiet
+                buttonLabelBehavior="hide"
+                overflowMode="collapse"
+                flex="0 0 auto"
+                maxWidth={40}
+                width={40}
+                onAction={(action) => {
+                    switch (action) {
+                        case "duplicate":
+                            duplicate();
+                            break;
+                        case "moveUp":
+                            reorder("forward");
+                            break;
+                        case "moveDown":
+                            reorder("backward");
+                            break;
+                        default:
+                            return;
+                    }
+                }}
+            >
+                <Item key="duplicate">
+                    <Duplicate />
+                    <Text>Duplicate</Text>
+                </Item>
+                <Item key="moveUp">
+                    <ChevronUp />
+                    <Text>Bring Forward</Text>
+                </Item>
+                <Item key="moveDown">
+                    <ChevronDown />
+                    <Text>Send Backward</Text>
+                </Item>
+            </ActionGroup>
+        </Flex>
     );
 };
