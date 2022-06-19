9563e2b0999e85405611cfe64df59590a3133141
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-06-19 12:59:33 -0500

    Hide navigator panel on click away

--- matico_components/src/Components/MaticoEditor/EditorComponents/NavigatorBar/NavigatorBar.tsx
+++ matico_components/src/Components/MaticoEditor/EditorComponents/NavigatorBar/NavigatorBar.tsx
@@ -15,86 +15,75 @@ import { DatasetsEditor } from "Components/MaticoEditor/Panes/DatasetsEditor";
 import { MaticoOutlineViewer } from "Components/MaticoEditor/Panes/MaticoOutlineViewer";
 import { Editors } from "Components/MaticoEditor/Editors";
 import { AppEditor } from "Components/MaticoEditor/Panes/AppEditor";
+import ClickAwayListener from 'react-click-away-listener';
 
-const panelSizes = [
-    {
-        icon: '|',
-        size: 10
-    },
-    {
-        icon: '||',
-        size: 20
-    },
-    {
-        icon: '|||',
-        size: 40
-    },
-    {
-        icon: '||||',
-        size: 60
-    }
-]
 
 export const NavigatorBar: React.FC<NavigatorBarProps> = ({
     datasetProviders
 }) => {
     const [showPanel, setShowPanel] = useState<boolean>(true);
 
-    const togglePanel = () => setShowPanel(p => !p)
+    const handleShowPanel = () => setShowPanel(true);
+    const handleHidePanel = () => setShowPanel(false);
+
     const { spec, currentEditPath, currentEditType } = useMaticoSelector(
         (state) => state.spec
     );
 
     // @ts-ignore
     const EditPane = currentEditPath ? Editors[currentEditType] : AppEditor;
-    return <View position="relative" maxHeight="100vh" overflow="visible" paddingTop="3em">
-        <Tabs
-            aria-labelledby="label-3"
-            orientation="vertical">
 
-            <TabList>
-                <Item key="components"><Pattern size="XL" /></Item>
-                <Item key="outline"><Layers size="XL" /></Item>
-                <Item key="datasets"><Data size="L" /></Item>
-                <Item key="dataviews"><Curate size="L" /></Item>
-                <Item key="state"><Channel size="L" /></Item>
-                <Item key="spec"><DocumentOutline size="L" /></Item>
-            </TabList>
-            <View
-                position="absolute"
-                left="100%"
-                top="3em"
-                height="calc(100vh - 3em)"
-                overflow="hidden"
-                minWidth={`20em`}
-                zIndex={500}
-
-            >
-                <View
-                    position="relative"
-                    width="100%"
-                    height="100%"
-                    overflow="hidden auto"
-                    backgroundColor={"gray-75"}
-                    UNSAFE_style={{
-                        resize: 'horizontal'
-                    }}
-                >
-                    <TabPanels
-                        width="100%"
-                        height="calc(100% - 2em)"
+    return <ClickAwayListener onClickAway={handleHidePanel}>
+        <div>
+            <View position="relative" maxHeight="100vh" overflow="visible" paddingTop="3em">
+                <Tabs
+                    aria-labelledby="label-3"
+                    orientation="vertical"
+                    onSelectionChange={handleShowPanel}
                     >
-                        <Item key="datasets"> <DatasetsEditor datasetProviders={datasetProviders} /></Item>
-                        <Item key="outline"> <MaticoOutlineViewer /></Item>
-                        <Item key="spec"> <MaticoRawSpecEditor /></Item>
-                        <Item key="state"> <MaticoStateViewer /></Item>
-                        <Item key="dataviews"> <p>Data views, coming soon...</p></Item>
-                        <Item key="components"> <EditPane editPath={currentEditPath} /></Item>
-                    </TabPanels>
-                </View>
+                    <TabList>
+                        <Item key="components"><Pattern size="XL" /></Item>
+                        <Item key="outline"><Layers size="XL" /></Item>
+                        <Item key="datasets"><Data size="L" /></Item>
+                        <Item key="dataviews"><Curate size="L" /></Item>
+                        <Item key="state"><Channel size="L" /></Item>
+                        <Item key="spec"><DocumentOutline size="L" /></Item>
+                    </TabList>
+                    <View
+                        position="absolute"
+                        left="100%"
+                        top="3em"
+                        height="calc(100vh - 3em)"
+                        overflow="hidden"
+                        minWidth={`20em`}
+                        zIndex={500}
+
+                    >
+                        {showPanel && <View
+                            position="relative"
+                            width="100%"
+                            height="100%"
+                            overflow="hidden auto"
+                            backgroundColor={"gray-75"}
+                            UNSAFE_style={{
+                                resize: 'horizontal'
+                            }}
+                        >
+                            <TabPanels
+                                width="100%"
+                                height="calc(100% - 2em)"
+                            >
+                                <Item key="datasets"> <DatasetsEditor datasetProviders={datasetProviders} /></Item>
+                                <Item key="outline"> <MaticoOutlineViewer /></Item>
+                                <Item key="spec"> <MaticoRawSpecEditor /></Item>
+                                <Item key="state"> <MaticoStateViewer /></Item>
+                                <Item key="dataviews"> <p>Data views, coming soon...</p></Item>
+                                <Item key="components"> <EditPane editPath={currentEditPath} /></Item>
+                            </TabPanels>
+                        </View>}
+                    </View>
+                </Tabs>
             </View>
-
-        </Tabs>
-    </View>
-
+        </div>
+    </ClickAwayListener>
 }
