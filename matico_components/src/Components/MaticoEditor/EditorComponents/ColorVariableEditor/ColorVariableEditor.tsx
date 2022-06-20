576d31c1f2d117f590e16f2d882e9e7da1852e8a
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-06-20 18:58:44 -0500

    Refactor color variable editor

--- matico_components/src/Components/MaticoEditor/EditorComponents/ColorVariableEditor/ColorVariableEditor.tsx
+++ matico_components/src/Components/MaticoEditor/EditorComponents/ColorVariableEditor/ColorVariableEditor.tsx
@@ -1,38 +1,29 @@
 import React from "react";
 import { ColorVariableEditorProps } from "./types";
 import _ from "lodash";
-import {
-  Flex,
-  Item,
-  Text,
-  Section,
-  ComboBox,
-} from "@adobe/react-spectrum";
-import RemoveCircle from '@spectrum-icons/workflow/RemoveCircle';
-import ColorFill from '@spectrum-icons/workflow/ColorFill';
-import Abc from '@spectrum-icons/workflow/ABC';
-import OneTwoThree from '@spectrum-icons/workflow/123';
 import { TwoUpCollapsableGrid } from "Components/MaticoEditor/Utils/TwoUpCollapsableGrid";
 import { DataDrivenModal } from "Components/MaticoEditor/Utils/DataDrivenModal";
 import { ColorPickerDialog } from "Components/MaticoEditor/Utils/ColorPickerDialog";
-
+import { ManualVariableComboBox } from "../ManualVariableComboBox";
+import ColorFill from "@spectrum-icons/workflow/ColorFill";
 
 export const ColorVariableEditor: React.FC<ColorVariableEditorProps> = ({
   label,
-  style,
+  style=[255,255,255],
   datasetName,
   columns,
   onUpdateStyle,
 }) => {
-
-  const isDataDriven = style.hasOwnProperty("variable");
-  const isNoColor = !isDataDriven && JSON.stringify(style) === '[0,0,0,0]'
-  const isManual = (!isDataDriven && !isNoColor) && (typeof style === 'string' || Array.isArray(style))
+  console.log(style)
+  const isDataDriven = style && style.hasOwnProperty("variable");
+  const isNone = !isDataDriven && JSON.stringify(style) === '[0,0,0,0]'
+  const isManual = (!isDataDriven && !isNone) && (typeof style === 'string' || Array.isArray(style))
 
   const handleComboBoxChange = (newVal: string) => {
-    if (newVal === "manualColor") {
+    console.log(newVal)
+    if (newVal === `manual${label}`) {
       onUpdateStyle(isManual ? style : [120, 120, 255]);
-    } else if (newVal === "noColor") {
+    } else if (newVal === `no${label}`) {
       onUpdateStyle([0, 0, 0, 0]);
     } else if (newVal.includes("datadriven-")) {
       const selectedColumn = newVal.split("datadriven-")[1]
@@ -58,44 +49,17 @@ export const ColorVariableEditor: React.FC<ColorVariableEditorProps> = ({
         alignItems: "end"
       }}
     >
-      <ComboBox
-        onSelectionChange={handleComboBoxChange}
+      <ManualVariableComboBox
         label={label}
-        width="100%"
-        // @ts-ignore
-        defaultInputValue={isDataDriven ? style?.variable : isNoColor ? "No Color" : isManual ? "Select Color" : ""}
-      >
-        <Section title="Manual">
-          <Item textValue="Select Color" key="manualColor">
-            <Flex gridArea={"text"} alignItems="center" gap=".25em">
-              <ColorFill size="S" />
-              <Text flexGrow={1}>
-                Select Color
-              </Text>
-            </Flex>
-          </Item>
-          <Item textValue="No Color" key="noColor">
-            <Flex gridArea={"text"} alignItems="center" gap=".25em">
-              <RemoveCircle size="S" />
-              <Text flexGrow={1}>
-                No Color
-              </Text>
-            </Flex>
-          </Item>
-        </Section>
-        <Section title="Data Driven">
-          {columns.map(({ name, type }, i) => (
-            <Item key={`datadriven-${name}`} textValue={name}>
-              <Flex gridArea={"text"} alignItems="center" gap=".25em">
-                {type === "number" ? <OneTwoThree size="S" /> : <Abc size="S" />}
-                <Text flexGrow={1}>
-                  {name}
-                </Text>
-              </Flex>
-            </Item>
-          ))}
-        </Section>
-      </ComboBox>
+        columns={columns}
+        style={style}
+        onChange={handleComboBoxChange}
+        manualIcon={<ColorFill size="S" />}
+        isManual={isManual}
+        isDataDriven={isDataDriven}
+        isNone={isNone}
+      />
+
       {!!isDataDriven && (
         <DataDrivenModal
           rangeType="color"
