014bde3dbf7523671d450861b0f990b4c10e8a94
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-06-20 13:07:43 -0500

    Refactor histogram pane

--- matico_components/src/Components/MaticoEditor/Panes/HistogramPaneEditor.tsx
+++ matico_components/src/Components/MaticoEditor/Panes/HistogramPaneEditor.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import _ from "lodash";
 import { useMaticoSelector } from "Hooks/redux";
-import { Well, Text, Heading, Flex, View } from "@adobe/react-spectrum";
+import { Well, Text, Heading, Flex, View, Divider } from "@adobe/react-spectrum";
 import { DatasetSelector } from "../Utils/DatasetSelector";
 import { DatasetColumnSelector } from "../Utils/DatasetColumnSelector";
 import { PaneEditor } from "./PaneEditor";
@@ -11,6 +11,7 @@ import { LabelEditor } from "../Utils/LabelEditor";
 import { TwoUpCollapsableGrid } from "../Utils/TwoUpCollapsableGrid";
 import { useMaticoSpec } from "Hooks/useMaticoSpec";
 import { useSpecActions } from "Hooks/useSpecActions";
+import { CollapsibleSection } from "../EditorComponents/CollapsibleSection";
 
 export interface PaneEditorProps {
     editPath: string;
@@ -70,54 +71,51 @@ export const HistogramPaneEditor: React.FC<PaneEditorProps> = ({
 
     return (
         <Flex direction="column">
-            <PaneEditor
-                position={histogramPane.position}
-                name={histogramPane.name}
-                background={histogramPane.background}
-                onChange={(change) => updatePaneDetails(change)}
-                parentLayout={parentLayout}
-            />
-            <Well>
-                <Heading>Data Source</Heading>
-                <TwoUpCollapsableGrid>
-                    <DatasetSelector
-                        selectedDataset={histogramPane.dataset.name}
-                        onDatasetSelected={updateDataset}
-                    />
-                    <DatasetColumnSelector
-                        datasetName={histogramPane.dataset.name}
-                        selectedColumn={histogramPane.column}
-                        label="Column"
-                        onColumnSelected={(column) => updateColumn(column.name)}
-                    />
-                </TwoUpCollapsableGrid>
-            </Well>
+            <CollapsibleSection title="Sizing" isOpen={true}>
+                <PaneEditor
+                    position={histogramPane.position}
+                    name={histogramPane.name}
+                    background={histogramPane.background}
+                    onChange={(change) => updatePaneDetails(change)}
+                    parentLayout={parentLayout}
+                />
+            </CollapsibleSection>
+            <CollapsibleSection title="Data Source" isOpen={true}>
+                <DatasetSelector
+                    selectedDataset={histogramPane.dataset.name}
+                    onDatasetSelected={updateDataset}
+                />
+                <DatasetColumnSelector
+                    datasetName={histogramPane.dataset.name}
+                    selectedColumn={histogramPane.column}
+                    label="Column"
+                    onColumnSelected={(column) => updateColumn(column.name)}
+                />
+            </CollapsibleSection>
             {dataset && (
-                <>
-                    <Well>
-                        <Heading>Style</Heading>
-                        <NumericVariableEditor
-                            label="Number of bins"
-                            minVal={2}
-                            maxVal={100}
-                            style={histogramPane.maxbins}
-                            datasetName={histogramPane.dataset.name}
-                            onUpdateStyle={(maxbins) => updatePane({ maxbins })}
-                        />
+                <CollapsibleSection title="Chart Settings" isOpen={true}>
+                    <Text>Style</Text>
+                    <NumericVariableEditor
+                        label="Number of bins"
+                        minVal={2}
+                        maxVal={100}
+                        style={histogramPane.maxbins}
+                        datasetName={histogramPane.dataset.name}
+                        onUpdateStyle={(maxbins) => updatePane({ maxbins })}
+                    />
 
-                        <ColorVariableEditor
-                            label="Color"
-                            style={histogramPane.color}
-                            datasetName={histogramPane.dataset.name}
-                            onUpdateStyle={(color) => updatePane({ color })}
-                        />
-                    </Well>
-
+                    <ColorVariableEditor
+                        label="Color"
+                        style={histogramPane.color}
+                        datasetName={histogramPane.dataset.name}
+                        onUpdateStyle={(color) => updatePane({ color })}
+                    />
+                    <Divider />
                     <LabelEditor
                         labels={histogramPane.labels}
                         onUpdateLabels={updateLabels}
                     />
-                </>
+                </CollapsibleSection>
             )}
         </Flex>
     );
