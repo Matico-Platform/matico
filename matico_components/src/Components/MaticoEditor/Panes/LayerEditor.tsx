372ed418a6ee69649827237b7e8558a50de285f1
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-06-20 19:08:35 -0500

    Layer editor refactor (wip)

--- matico_components/src/Components/MaticoEditor/Panes/LayerEditor.tsx
+++ matico_components/src/Components/MaticoEditor/Panes/LayerEditor.tsx
@@ -19,12 +19,22 @@ import {
   StatusLight,
   Divider,
   Checkbox,
+  Section,
+  ComboBox,
 } from "@adobe/react-spectrum";
-import { ColorVariableEditor } from "../Utils/ColorVariableEditor";
+import { ColorVariableEditor } from "Components/MaticoEditor/EditorComponents/ColorVariableEditor";
 import { NumericVariableEditor } from "../Utils/NumericVariableEditor";
 import { FilterEditor } from "../Utils/FilterEditor";
 import { Filter } from "Datasets/Dataset";
 import { TwoUpCollapsableGrid } from "../Utils/TwoUpCollapsableGrid";
+import { sanitizeColor } from "Utils/sanitizeColor";
+import RemoveCircle from '@spectrum-icons/workflow/RemoveCircle';
+import ColorFill from '@spectrum-icons/workflow/ColorFill';
+import Abc from '@spectrum-icons/workflow/ABC';
+import OneTwoThree from '@spectrum-icons/workflow/123';
+import { SliderVariableEditor } from "../EditorComponents/SliderVariableEditor";
+import { CollapsibleSection } from "../EditorComponents/CollapsibleSection";
+import { SliderUnitSelector } from "../EditorComponents/SliderUnitSelector";
 
 export interface LayerEditorProps {
   editPath: string;
@@ -40,8 +50,12 @@ export const LayerEditor: React.FC<LayerEditorProps> = ({ editPath }) => {
   const dataset = useMaticoSelector(
     (state) => state.datasets.datasets[layer.source.name]
   );
+  const {
+    columns,
+    geomType,
+  } = dataset;
 
-  const isPoint = dataset?.geomType === GeomType.Point;
+  const isPoint = geomType === GeomType.Point;
 
   const updateStyle = (property: string, value: any) => {
     dispatch(
@@ -108,7 +122,7 @@ export const LayerEditor: React.FC<LayerEditorProps> = ({ editPath }) => {
     dataDriven: boolean,
     type: "color" | "number"
   ) => {
-    const variable = dataset.columns.find((c) => c["type"] === "number");
+    const variable = columns.find((c) => c["type"] === "number");
 
     if (dataDriven) {
       const dataSpecs = {
@@ -150,13 +164,9 @@ export const LayerEditor: React.FC<LayerEditorProps> = ({ editPath }) => {
     }
   };
 
-  const fillDataDriven = style.fillColor.variable ? "Data Driven" : "Simple";
-  const lineDataDriven = style.lineColor.variable ? "Data Driven" : "Simple";
-
   return (
     <Flex direction="column">
-      <Well>
-        <Heading>Datasource</Heading>
+      <CollapsibleSection title="Datasource" isOpen={true}>
         <DatasetSelector
           selectedDataset={dataset.name}
           onDatasetSelected={updateDataset}
@@ -166,51 +176,88 @@ export const LayerEditor: React.FC<LayerEditorProps> = ({ editPath }) => {
           filters={layer.source.filters}
           onUpdateFilters={updateFilters}
         />
-      </Well>
-      <Well>
-        <Flex direction="column" gap="size-200">
-          <Heading margin="size-0">Style</Heading>
-          {/* *** LINE SETTING *** */}
-          <Flex gap="size-125" marginTop="size-200">
-            <Divider orientation="vertical" />
-            <Heading margin="size-0">Line</Heading>
-          </Flex>
-          <ColorVariableEditor
-            label="Line Color"
-            datasetName={dataset.name}
-            style={style.lineColor}
-            onUpdateStyle={(style) => {
-              updateStyle("lineColor", style);
+      </CollapsibleSection>
+      {dataset?.geomType !== GeomType.Line && <CollapsibleSection title="Fill" isOpen={true}>
+        <ColorVariableEditor
+          label="Fill Color"
+          datasetName={dataset.name}
+          columns={columns}
+          style={style.fillColor}
+          onUpdateStyle={(style) => {
+            console.log('FILLCOLOR', style)
+            updateStyle("fillColor", style);
+          }}
+        />
+      </CollapsibleSection>}
+      {dataset?.geomType === GeomType.Point && <CollapsibleSection title="Radius" isOpen={true}>
+        <NumericVariableEditor
+          label="Point Radius"
+          datasetName={dataset.name}
+          style={style.size}
+          onUpdateStyle={(style) => updateStyle("size", style)}
+          minVal={0}
+          maxVal={2000}
+        />
+        <Flex
+          alignContent={"end"}
+          justifyContent={"space-between"}
+        >
+          <NumberField
+            value={style.radiusScale}
+            width="size-1200"
+            label="Point Radius Multiplier"
+            maxValue={100000}
+            minValue={1}
+            onChange={(val) => updateStyle("radiusScale", val)}
+          />
+          <Picker
+            width="size-1200"
+            alignSelf={"flex-end"}
+            label="Point Radius Units"
+            selectedKey={style.radiusUnits}
+            onSelectionChange={(units) =>
+              updateStyle("radiusUnits", units as string)
+            }
+          >
+            <Item key="meters">Meters</Item>
+            <Item key="pixels">Pixels</Item>
+          </Picker>
+        </Flex>
+      </CollapsibleSection>}
+      <CollapsibleSection title="Line" isOpen={true}>
+        <ColorVariableEditor
+          label="Line Color"
+          style={style.lineColor}
+          datasetName={dataset.name}
+          columns={columns}
+          onUpdateStyle={(style) => updateStyle("lineColor", style)}
+        />
+        <SliderVariableEditor
+          label="Line Width"
+          style={style.lineWidth}
+          datasetName={dataset.name}
+          columns={columns}
+          onUpdateValue={(style) => updateStyle("lineWidth", style)}
+        />
+        {style.lineWidth !== -1 && (
+          <TwoUpCollapsableGrid
+            gridProps={{
+              marginTop: "size-100"
             }}
-          />
-          <NumericVariableEditor
-            label="Line Width"
-            datasetName={dataset.name}
-            style={style.lineWidth}
-            onUpdateStyle={(style) => updateStyle("lineWidth", style)}
-            minVal={0}
-            maxVal={2000}
-          // picker={true} TODO: add picker integrated into numberic editor
-          // pickerKey={style.lineUnits}
-          // pickerOptions={[{ label: "px", value: "px" }, { label: "pt", value: "pt" }]}
-          // onPickerChange={(units) =>
-          //   updateStyle("lineUnits", units as string)
-          // }
-          />
-          <Flex
-            alignContent={"end"}
-            justifyContent={"space-between"}
           >
             <NumberField
               value={style.lineWidthScale}
-              width="size-1200"
-              label="Line Width Multiplier"
-              maxValue={100}
+              label="Multiplier"
+              aria-label="Line width multiplier"
+              labelPosition="side"
+              maxValue={10000}
               minValue={1}
               onChange={(val) => updateStyle("lineWidthScale", val)}
             />
             <Picker
-              label="Line Width Units"
+              label="Units"
+              aria-label="Line width units"
+              labelPosition="side"
               alignSelf={"flex-end"}
               selectedKey={style.lineUnits}
               onSelectionChange={(units) => updateStyle("lineUnits", units)}
@@ -218,114 +265,47 @@ export const LayerEditor: React.FC<LayerEditorProps> = ({ editPath }) => {
               <Item key="meters">Meters</Item>
               <Item key="pixels">Pixels</Item>
             </Picker>
-          </Flex>
-          {/* *** POINT SETTINGS *** */}
-          {dataset?.geomType === GeomType.Point && <>
-            <Flex gap="size-125" marginTop="size-200">
-              <Divider orientation="vertical" />
-              <Heading margin="size-0" marginTop="size">Point Sizing</Heading>
-            </Flex>
-            <NumericVariableEditor
-              label="Point Radius"
-              datasetName={dataset.name}
-              style={style.size}
-              onUpdateStyle={(style) => updateStyle("size", style)}
-              minVal={0}
-              maxVal={2000}
-            />
-            <Flex
-              alignContent={"end"}
-              justifyContent={"space-between"}
-            >
-              <NumberField
-                value={style.radiusScale}
-                width="size-1200"
-                label="Point Radius Multiplier"
-                maxValue={100000}
-                minValue={1}
-                onChange={(val) => updateStyle("radiusScale", val)}
-              />
-              <Picker
-                width="size-1200"
-                alignSelf={"flex-end"}
-                label="Point Radius Units"
-                selectedKey={style.radiusUnits}
-                onSelectionChange={(units) =>
-                  updateStyle("radiusUnits", units as string)
-                }
-              >
-                <Item key="meters">Meters</Item>
-                <Item key="pixels">Pixels</Item>
-              </Picker>
-            </Flex>
-          </>}
-
-          {/* *** FILL *** */}
-          {dataset?.geomType !== GeomType.Line && <>
-            <Flex gap="size-125" marginTop="size-200">
-              <Divider orientation="vertical" />
-              <Heading margin="size-0" marginTop="size">Fill</Heading>
-            </Flex>
-            <ColorVariableEditor
-              label="Fill Color"
-              datasetName={dataset.name}
-              style={style.fillColor}
-              onUpdateStyle={(style) => {
-                updateStyle("fillColor", style);
-              }}
-            />
-          </>}
-          {/* *** 3D *** */}
-          {dataset?.geomType === GeomType.Polygon && <>
-            <Flex gap="size-125" marginTop="size-200">
-              <Divider orientation="vertical" />
-              <Heading margin="size-0" marginTop="size">3D</Heading>
-            </Flex>
-            <NumericVariableEditor
-              label="Elevation"
-              datasetName={dataset.name}
-              style={style.elevation}
-              onUpdateStyle={(style) => updateStyle("elevation", style)}
-              minVal={0}
-              maxVal={10000}
-            />
-            <NumberField
-              value={style.elevationScale}
-              width="size-1200"
-              label="Elevation Multiplier"
-              maxValue={100000}
-              minValue={1}
-              onChange={(val) => updateStyle("elevationScale", val)}
-            />
-          </>}
-          {/* *** VISIBILITY *** */}
-          <Flex gap="size-125" marginTop="size-200">
-            <Divider orientation="vertical" />
-            <Heading margin="size-0" marginTop="size">Visibility</Heading>
-          </Flex>
-
-          <Flex
-            alignContent={"end"}
-            justifyContent={"space-between"}
+          </TwoUpCollapsableGrid>)}
+      </CollapsibleSection>
+      {dataset?.geomType === GeomType.Polygon && <CollapsibleSection title="3D" isOpen={true}>
+        <SliderVariableEditor
+          label="Elevation"
+          style={style.elevation}
+          datasetName={dataset.name}
+          columns={columns}
+          onUpdateValue={(style) => updateStyle("elevation", style)}
+          sliderMin={0}
+          sliderMax={10000}
+        />
+        <TwoUpCollapsableGrid>
+          <NumberField
+            value={style.elevationScale}
+            width="100%"
+            label="Elevation Multiplier"
+            maxValue={100000}
+            minValue={1}
+            onChange={(val) => updateStyle("elevationScale", val)}
+          />
+        </TwoUpCollapsableGrid>
+      </CollapsibleSection>}
+      <CollapsibleSection title="Opacity and Visibility" isOpen={true}>
+        <TwoUpCollapsableGrid>
+          <SliderUnitSelector
+            label="Layer Opacity"
+            value={style.opacity === undefined ? 1 : style.opacity}
+            sliderMin={0}
+            sliderMax={1}
+            sliderStep={.01}
+            onUpdateValue={(val) => updateStyle("opacity", val)}
+          />
+          <Checkbox
+            isSelected={style.visible === undefined ? true : style.visible}
+            onChange={() => updateStyle("visible", !style.visible)}
           >
-            <Slider
-              value={style.opacity}
-              label="Layer Opacity"
-              onChange={(val) => updateStyle("opacity", val)}
-              maxValue={1}
-              minValue={0}
-              step={.01}
-              showValueLabel={true}
-            />
-            <Checkbox
-              isSelected={!!style.visible}
-              onChange={() => updateStyle("visible", !style.visible)}
-            >
-              Layer Visibility
-            </Checkbox>
-          </Flex>
-        </Flex>
-      </Well>
-    </Flex>
+            Layer Visibility
+          </Checkbox>
+        </TwoUpCollapsableGrid>
+      </CollapsibleSection>
+    </Flex >
   );
 };
