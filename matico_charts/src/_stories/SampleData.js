7163d0bcc38e2843a8eed8d6fe104a6a3487bac1
bkbrown226 <102566047+bkbrown226@users.noreply.github.com>
2022-07-27 11:41:03 -0500

    Ready to pull

--- matico_charts/src/_stories/SampleData.js
+++ matico_charts/src/_stories/SampleData.js
@@ -10,12 +10,8 @@ const Sample2dData = generate2dData(1000)
 const SampleRegressionLineData = getRegressionLine(Sample2dData)
 const SampleHistogramData = getHistogramData(Sample2dData)
 const SampleCategoricalData = getCategoricalData(1000)
-<<<<<<< HEAD
 const SampleMapData= getMapData(1)
 const SampleMapData2= getMapData(2)
-=======
-const SampleMapData = getMapData()
->>>>>>> 2747b0a5 (Ready to receive Chen's push)
 
 export {
     Sample2dData,
