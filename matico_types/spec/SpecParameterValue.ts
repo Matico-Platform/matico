8fcef73c78da54ddec46d75ba341f5044a27b2c0
Stuart Lynn <stuart.lynn@gmail.com>
2022-07-27 13:38:21 -0400

    Types for Spec Parameter Value

--- matico_types/spec/SpecParameterValue.ts
+++ matico_types/spec/SpecParameterValue.ts
@@ -2,4 +2,4 @@
 import type { SpecParameter } from "./SpecParameter";
 import type { VarOr } from "./VarOr";
 
-export type SpecParameterValue = { type: "optionGroup", value: Array<VarOr<SpecParameter>> } | { type: "repeatedOption", value: Array<VarOr<SpecParameterValue>> } | { type: "numericFloat", value: VarOr<number> } | { type: "numericInt", value: VarOr<number> } | { type: "numericCategory", value: VarOr<Array<number>> } | { type: "textCategory", value: VarOr<Array<string>> } | { type: "column", value: VarOr<string> } | { type: "table", value: VarOr<string> } | { type: "text", value: VarOr<string> };
+export type SpecParameterValue = { type: "optionGroup", value: Array<SpecParameter> } | { type: "repeatedOption", value: Array<SpecParameterValue> } | { type: "numericFloat", value: VarOr<number> } | { type: "numericInt", value: VarOr<number> } | { type: "numericCategory", value: VarOr<Array<number>> } | { type: "textCategory", value: VarOr<Array<string>> } | { type: "column", value: VarOr<string> } | { type: "table", value: VarOr<string> } | { type: "text", value: VarOr<string> };
