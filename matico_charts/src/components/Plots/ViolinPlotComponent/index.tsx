8e656c7621c18fbd1d286343d9b7c25f5b9a8383
chenjia-lin <chenjia.lin2436@gmail.com>
2022-08-02 14:54:58 -0500

    Adding folder for ViolinPlotComponent

---
+++ matico_charts/src/components/Plots/ViolinPlotComponent/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Group } from '@visx/group';
+import { ViolinPlot, BoxPlot } from '@visx/stats';
+import { LinearGradient } from '@visx/gradient';
+import { scaleBand, scaleLinear } from '@visx/scale';
+import genStats, { Stats } from '@visx/mock-data/lib/generators/genStats';
+import { getSeededRandom, getRandomNormal } from '@visx/mock-data';
+import { withTooltip, Tooltip, defaultStyles as defaultTooltipStyles } from '@visx/tooltip';
+import { WithTooltipProvidedProps } from '@visx/tooltip/lib/enhancers/withTooltip';
+import { PatternLines } from '@visx/pattern';
