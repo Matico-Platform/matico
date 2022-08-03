4f5bf8cb413a0953b69cba27967333160ebaac24
chenjia-lin <chenjia.lin2436@gmail.com>
2022-08-03 13:41:09 -0500

    Some local changes to the index.tsx for violin-plot-pane

--- matico_charts/src/components/Plots/ViolinPlotComponent/index.tsx
+++ matico_charts/src/components/Plots/ViolinPlotComponent/index.tsx
@@ -8,3 +8,40 @@ import { getSeededRandom, getRandomNormal } from '@visx/mock-data';
 import { withTooltip, Tooltip, defaultStyles as defaultTooltipStyles } from '@visx/tooltip';
 import { WithTooltipProvidedProps } from '@visx/tooltip/lib/enhancers/withTooltip';
 import { PatternLines } from '@visx/pattern';
+
+// accessors
+const x = (d: Stats) => d.boxPlot.x;
+const min = (d: Stats) => d.boxPlot.min;
+const max = (d: Stats) => d.boxPlot.max;
+const median = (d: Stats) => d.boxPlot.median;
+const firstQuartile = (d: Stats) => d.boxPlot.firstQuartile;
+const thirdQuartile = (d: Stats) => d.boxPlot.thirdQuartile;
+
+interface TooltipData {
+    name?: string;
+    min?: number;
+    median?: number;
+    max?: number;
+    firstQuartile?: number;
+    thirdQuartile?: number;
+}
+
+export type StatsPlotProps = {
+    width: number;
+    height: number;
+}
+
+export default withTooltip<StatsPlotProps, TooltipData>(
+    ({
+        width,
+        height,
+        tooltipOpen,
+        tooltipLeft,
+        tooltipTop,
+        tooltipData,
+        showTooltip,
+        hideTooltip
+    }: StatsPlotProps & WithTooltipProvidedProps<TooltipData>) => {
+
+    }
+)
