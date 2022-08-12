e0feb17e7f86027139cff6c978d02161913f45ce
bkbrown226 <102566047+bkbrown226@users.noreply.github.com>
2022-08-12 11:30:42 -0500

    Fixed a typo in index.ts within Plots

--- matico_charts/src/components/Plots/index.ts
+++ matico_charts/src/components/Plots/index.ts
@@ -2,7 +2,7 @@ import { ScatterplotComponent } from './ScatterplotComponent';
 import { BarComponent } from './BarComponent';
 import { LineComponent } from './LineComponent';
 import { PieChartComponent } from './PieChartComponent';
-import { DistributionPlotComponent } from './ViolinPlotComponent';
+import { DistributionPlotComponent } from './DistributionPlotComponent';
 // import { HeatmapComponent } from './HeatmapComponent';
 
 const PlotComponentMapping = {
