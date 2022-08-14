34265568d261390a4687ee4d57aaeb5b3a694cfb
Stuart Lynn <stuart.lynn@gmail.com>
2022-08-14 18:51:15 -0400

    Reducing max point size for data driven styling on Scatterplot

--- matico_components/src/Components/MaticoEditor/Panes/ScatterPlotPaneEditor.tsx
+++ matico_components/src/Components/MaticoEditor/Panes/ScatterPlotPaneEditor.tsx
@@ -110,7 +110,7 @@ export const ScatterplotPaneEditor: React.FC<PaneEditorProps> = ({
                     columns={dataset?.columns}
                     onUpdateValue={(dotSize) => updatePane({ dotSize })}
                     sliderMin={0}
-                    sliderMax={2000}
+                    sliderMax={10}
                 />
 
                 <ColorVariableEditor
