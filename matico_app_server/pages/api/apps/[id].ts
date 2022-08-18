e91292327d8838aba5f538e3d85c4e5f82c7a11f
Stuart Lynn <stuart.lynn@gmail.com>
2022-08-18 15:46:44 -0400

    Getting ready for deploy

--- matico_app_server/pages/api/apps/[id].ts
+++ matico_app_server/pages/api/apps/[id].ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+import {NextApiRequest, NextApiResponse} from "next";
 import { unstable_getServerSession } from "next-auth";
 import { userFromSession, userHasView } from "../../../utils/db";
 import { authOptions } from "../auth/[...nextauth]";
 
-export default async function handler(req, res) {
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
   const prisma = new PrismaClient();
 
   const session = await unstable_getServerSession(req, res, authOptions);
