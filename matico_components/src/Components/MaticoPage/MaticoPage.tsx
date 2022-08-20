fed3375184d24b506d0599879953dc1e2eea0d72
Dylan <dhalpern@uchicago.edu>
2022-08-20 16:34:07 -0500

    Remove dnd from matico page

--- matico_components/src/Components/MaticoPage/MaticoPage.tsx
+++ matico_components/src/Components/MaticoPage/MaticoPage.tsx
@@ -2,13 +2,31 @@ import React, { useRef } from "react";
 import { Page, Layout } from "@maticoapp/matico_types/spec";
 import { View, Flex } from "@adobe/react-spectrum";
 import { selectLayout } from "Utils/layoutEngine";
-import {usePage} from "Hooks/usePage";
+import { usePage } from "Hooks/usePage";
 import { useIsEditable } from "Hooks/useIsEditable";
-import { DndContext, KeyboardSensor, MouseSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core";
-import { coordinateGetter } from "Components/MaticoEditor/EditorComponents/SortableDraggableList/multipleContainersKeyboardCoordinates";
+import {
+    DndContext,
+    KeyboardSensor,
+    MouseSensor,
+    TouchSensor,
+    useDroppable,
+    useSensor,
+    useSensors
+} from "@dnd-kit/core";
 import { ParentProvider } from "Hooks/useParentContext";
 import styled from "styled-components";
-import { restrictToParentElement } from "@dnd-kit/modifiers";
+import {
+    restrictToParentElement,
+    restrictToWindowEdges
+} from "@dnd-kit/modifiers";
+import { DraggingProvider, useDraggingContext } from "Components/MaticoEditor/Panes/MaticoOutlineViewer/DraggingContext";
+import { handleDrag } from "Utils/dragAndResize/handleDrag";
+import { useApp } from "Hooks/useApp";
+import {
+    layoutCollisionDetection,
+    outlineCollisionDetection
+} from "Components/MaticoEditor/Panes/MaticoOutlineViewer/CollisionDetection";
+import { ContainerDropTarget } from "Components/MaticoEditor/Panes/MaticoOutlineViewer/ContainerDropTarget";
 interface MaticoPageInterface {
     pageId: string;
 }
@@ -17,50 +35,40 @@ const PageParent = styled.div`
     width: 100%;
     height: 100%;
     overflow: none auto;
-`
+`;
 
 export const MaticoPage: React.FC<MaticoPageInterface> = ({ pageId }) => {
-    let {page} = usePage(pageId)
+    let { page } = usePage(pageId);
     let layout: Layout = page?.layout;
     let LayoutEngine = selectLayout(layout);
+
     const parentRef = useRef<HTMLDivElement>(null);
-    const isEdit = useIsEditable();
-    const Wrapper = isEdit ? DndContext : React.Fragment;
 
-    const handleDragStart = (event: any) => {
-        // console.log("drag start", event);
-    }
-    const handleDragEnd = (event: any) => {
-        // console.log("drag end", event);
-    }
-
-    const sensors = useSensors(
-        useSensor(MouseSensor),
-        useSensor(TouchSensor),
-        useSensor(KeyboardSensor, {
-            coordinateGetter
-        })
-    );
-
-
+    const { isOver, setNodeRef } = useDroppable({
+        id: page.id,
+        data: {
+            targetId: page.id,
+            type: "page",
+            depth: 0
+        }
+    });
+    const activeItem = useDraggingContext();
+    const showDropZone = // @ts-ignore
+        !!activeItem && activeItem?.data?.current?.type !== "page";
 
     return (
-        <Wrapper
-            // @ts-ignore
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-            // onDragOver={handleDragOver}
-            // collisionDetection={collisionDetectionStrategy}
-            sensors={sensors}
-            modifiers={[restrictToParentElement]}
-        >
-            <PageParent ref={parentRef}>
-                <ParentProvider parentRef={parentRef}>
-                <Flex direction="column" width={"100%"} height={"100%"}>
-                    <LayoutEngine paneRefs={page?.panes} />
-                </Flex>
-                </ParentProvider>
-            </PageParent>
-        </Wrapper>
+        <PageParent ref={parentRef}>
+            <ParentProvider parentRef={parentRef}>
+                <ContainerDropTarget
+                    ref={setNodeRef}
+                    active={showDropZone}
+                    isOver={isOver}
+                >
+                    <Flex direction="column" width={"100%"} height={"100%"}>
+                        <LayoutEngine paneRefs={page?.panes} />
+                    </Flex>
+                </ContainerDropTarget>
+            </ParentProvider>
+        </PageParent>
     );
 };
