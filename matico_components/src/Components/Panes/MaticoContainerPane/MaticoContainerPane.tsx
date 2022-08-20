a1651c4f9d516a0d6116c878ec1434136ae80e94
Dylan <dhalpern@uchicago.edu>
2022-08-20 18:31:27 -0500

    Container pane dnd target

--- matico_components/src/Components/Panes/MaticoContainerPane/MaticoContainerPane.tsx
+++ matico_components/src/Components/Panes/MaticoContainerPane/MaticoContainerPane.tsx
@@ -4,31 +4,55 @@ import { useIsEditable } from "../../../Hooks/useIsEditable";
 import { Content, View } from "@adobe/react-spectrum";
 import { selectLayout } from "Utils/layoutEngine";
 import { Layout, PaneRef } from "@maticoapp/matico_types/spec";
+import { useDroppable } from "@dnd-kit/core";
+import { usePane } from "Hooks/usePane";
+import { ContainerDropTarget } from "Components/MaticoEditor/Panes/MaticoOutlineViewer/ContainerDropTarget";
+import { useDraggingContext } from "Components/MaticoEditor/Panes/MaticoOutlineViewer/DraggingContext";
 
 export interface MaticoContainerPaneInterface extends MaticoPaneInterface {
     title?: string;
     panes: PaneRef[];
     layout: Layout;
+    paneRef?: PaneRef;
 }
 
 export const MaticoContainerPane: React.FC<MaticoContainerPaneInterface> = ({
     layout,
-    panes
+    panes,
+    paneRef,
+    id
 }) => {
-
     const edit = useIsEditable();
-    let Layout= selectLayout(layout);
+    let Layout = selectLayout(layout);
+    const { isOver, setNodeRef } = useDroppable({
+        id: id,
+        data: {
+            targetId: id,
+            paneId: id,
+            paneRefId: paneRef.id,
+            type: "container"
+        }
+    });
+    const activeItem = useDraggingContext();
+    const showDropZone = // @ts-ignore
+        !!activeItem && activeItem?.data?.current?.type !== "page";
+
     return (
-        <View
-            position="relative"
-            overflow="hidden auto"
-            width="100%"
-            height="100%"
-            backgroundColor={edit ? "default" : "transparent"}
+        <ContainerDropTarget
+            style={{
+                position: "relative",
+                overflow: "hidden auto",
+                width: "100%",
+                height: "100%",
+                backgroundColor: edit ? undefined : "transparent"
+            }}
+            ref={setNodeRef}
+            isOver={isOver}
+            active={showDropZone}
         >
             <Content width="100%" height="100%">
-                <Layout paneRefs={panes} {...layout} />
+                <Layout paneRefs={panes} {...layout} paneRef={paneRef} />
             </Content>
-        </View>
+        </ContainerDropTarget>
     );
 };
