ccdb341e937ce75ac9a3aa4208d7c253789b38d9
Dylan <dhalpern@uchicago.edu>
2022-08-21 23:25:13 -0500

    Splash screen styling

--- matico_app_server/components/Header/Header.tsx
+++ matico_app_server/components/Header/Header.tsx
@@ -1,6 +1,14 @@
-import { Flex, Text, View } from "@adobe/react-spectrum";
+import { ActionButton, Button, Flex, Text, View } from "@adobe/react-spectrum";
+import { useSession } from "next-auth/react";
 import { Login } from "../Login/Login";
-export const Header: React.FC = () => {
+import Add from "@spectrum-icons/workflow/Add";
+import Link from "next/link";
+import { useApps } from "../../hooks/useApps";
+export const Header: React.FC<{
+  createNewApp?: (template: string) => void;
+}> = ({ createNewApp }) => {
+  const { data: session } = useSession();
+
   return (
     <View
       paddingY="size-200"
@@ -16,13 +24,58 @@ export const Header: React.FC = () => {
         justifyContent="space-between"
         width="100%"
       >
-        <Text UNSAFE_style={{
-          fontSize: "2em",
-          fontWeight: "bold"
-        }}>Matico</Text>
-        <View justifySelf="flex-end">
+        <Flex direction="row" gap="size-150" alignItems="center">
+          <img src="/logo.png" alt="Matico" width="32px" height="32px" />
+          <Text
+            UNSAFE_style={{
+              fontSize: "2em",
+              fontWeight: "bold",
+            }}
+          >
+            Matico
+          </Text>
+          <View marginStart="size-450" marginTop="size-50"></View>
+        </Flex>
+        <Flex
+          direction="row"
+          gap="size-450"
+          alignItems="center"
+          justifySelf="flex-end"
+        >
+          {(!!session && !!createNewApp) && (
+            <ActionButton
+              isQuiet
+              onPress={() => {
+                createNewApp('BigMap')
+              }}
+              UNSAFE_style={{
+                cursor: "pointer",
+              }}
+            >
+              <Add />
+              Start a New App
+            </ActionButton>
+          )}
           <Login />
-        </View>
+          <Link
+            href="https://matico.app"
+            target={"_blank"}
+            rel="noopener noreferrer"
+          >
+            <a target={"_blank"} rel="noopener noreferrer">
+              Docs
+            </a>
+          </Link>
+          <Link
+            href="https://github.com/matico-Platform/matico"
+            target={"_blank"}
+            rel="noopener noreferrer"
+          >
+            <a target={"_blank"} rel="noopener noreferrer">
+              Github
+            </a>
+          </Link>
+        </Flex>
       </Flex>
     </View>
   );
