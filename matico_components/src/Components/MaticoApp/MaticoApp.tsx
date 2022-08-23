85624ca46de352050d9a06a3acedd6c59f61032c
Dylan <dhalpern@uchicago.edu>
2022-08-23 10:10:30 -0500

    Fontawesome Macos Fix ?

--- matico_components/src/Components/MaticoApp/MaticoApp.tsx
+++ matico_components/src/Components/MaticoApp/MaticoApp.tsx
@@ -29,7 +29,7 @@ import { App } from "@maticoapp/matico_types/spec";
 import { SocrataDatasetProvider } from "DatasetsProviders/SocrataProvider/SocrataProvider";
 import { ComputeProvider } from "DatasetsProviders/ComputeProvider";
 
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./fa.min.css";
 import Visibility from "@spectrum-icons/workflow/Visibility";
 import VisibilityOff from "@spectrum-icons/workflow/VisibilityOff";
 import DeviceDesktop from "@spectrum-icons/workflow/DeviceDesktop";
