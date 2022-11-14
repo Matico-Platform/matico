7595b1a0983bf126c53834ad6663823c52daed48
Stuart Lynn <stuart.lynn@gmail.com>
2022-11-14 11:34:58 +0000

    format on new files

--- matico_common/src/compute/variables.rs
+++ matico_common/src/compute/variables.rs
@@ -177,65 +177,6 @@ pub struct SpecParameter {
     parameter: SpecParameterValue,
 }
 
-impl TryFrom<&ParameterValue> for i32 {
-    type Error = ArgError;
-
-    fn try_from(parameter_value: &ParameterValue) -> Result<i32, Self::Error> {
-        if let ParameterValue::NumericInt(val) = parameter_value {
-            return Ok(*val);
-        } else {
-            Err(ArgError::new("", "Failed to convert ParameterValue to i32"))
-        }
-    }
-}
-
-impl TryFrom<&ParameterValue> for u32 {
-    type Error = ArgError;
-
-    fn try_from(parameter_value: &ParameterValue) -> Result<u32, Self::Error> {
-        if let ParameterValue::NumericInt(val) = parameter_value {
-            return Ok(*val as u32);
-        } else {
-            Err(ArgError::new("", "Failed to convert ParameterValue to i32"))
-        }
-    }
-}
-
-impl TryFrom<&ParameterValue> for Vec<u32> {
-    type Error = ArgError;
-
-    fn try_from(parameter_value: &ParameterValue) -> Result<Vec<u32>, Self::Error> {
-        if let ParameterValue::NumericCategory(val) = parameter_value {
-            return Ok(val.clone());
-        } else {
-            Err(ArgError::new(
-                "",
-                "Failed to convert Numeric Category ParameterValue to  u32",
-            ))
-        }
-    }
-}
-
-impl TryFrom<&ParameterValue> for String {
-    type Error = ArgError;
-    fn try_from(parameter_value: &ParameterValue) -> Result<String, Self::Error> {
-        match parameter_value {
-            ParameterValue::Column(s) => Ok(s.clone()),
-            ParameterValue::Table(s) => Ok(s.clone()),
-            ParameterValue::Text(s) => Ok(s.clone()),
-            _ => Err(ArgError::new("", "Failed to convert ParameterValue to i32")),
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, TS)]
-#[serde(rename_all = "camelCase")]
-#[ts(export)]
-pub struct SpecParameter {
-    name: String,
-    parameter: SpecParameterValue,
-}
-
 #[derive(Serialize, Deserialize, Debug, Clone, TS)]
 #[serde(rename_all = "camelCase", tag = "type", content = "value")]
 #[ts(export)]
